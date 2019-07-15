5a22e36181bfa213788ded4f51c2bf1d5510ec24
Thulio Ferraz Assis <tassis@suse.com>
2019-07-15 15:18:03 -0700

    Fixed patch issues

--- bosh/releases/pre_render_scripts/api/cloud_controller_ng/remove_tee_output_to_sys_log.sh
+++ bosh/releases/pre_render_scripts/api/cloud_controller_ng/remove_tee_output_to_sys_log.sh
@@ -5,5 +5,5 @@ set -o errexit
 # TODO: Figure out why tee_output_to_sys_log fails the pre-start.
 patch /var/vcap/all-releases/jobs-src/capi/cloud_controller_ng/templates/pre-start.sh.erb <<'EOT'
 7d6
-< tee_output_to_sys_log "cloud_controller_ng.\$(basename "\$0")"
+< tee_output_to_sys_log "cloud_controller_ng.$(basename "$0")"
 EOT
