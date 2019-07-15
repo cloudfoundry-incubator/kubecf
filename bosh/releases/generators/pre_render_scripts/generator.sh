93856bee2bf6edbcfaa7731ec4ff9148c3c700aa
Thulio Ferraz Assis <tassis@suse.com>
2019-07-15 12:10:48 -0700

    Simplified pre_render_scripts generator

--- bosh/releases/generators/pre_render_scripts/generator.sh
+++ bosh/releases/generators/pre_render_scripts/generator.sh
@@ -4,11 +4,7 @@ set -o errexit -o nounset
 
 touch "${OUTPUT}"
 
-function append {
-  echo "${1}" >> "${OUTPUT}"
-}
-
-append "- type: replace"
-append "  path: /instance_groups/name=${INSTANCE_GROUP}/jobs/name=${JOB}/properties/bosh_containerization?/pre_render_scripts/-"
-append "  value: |"
-append "$(cat "${PRE_RENDER_SCRIPT}" | sed 's/^/    /')"
+echo "- type: replace" >> "${OUTPUT}"
+echo "  path: /instance_groups/name=${INSTANCE_GROUP}/jobs/name=${JOB}/properties/bosh_containerization?/pre_render_scripts/-" >> "${OUTPUT}"
+echo "  value: |" >> "${OUTPUT}"
+cat "${PRE_RENDER_SCRIPT}" | sed 's/^/    /' >> "${OUTPUT}"
