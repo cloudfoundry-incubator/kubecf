d24c5850c504d70684e7a69622b5a1a7f8b21c9e
Thulio Ferraz Assis <tassis@suse.com>
2019-07-18 09:32:49 -0700

    Fixed linting issues on pre_render_scripts generator

--- bosh/releases/generators/pre_render_scripts/generator.sh
+++ bosh/releases/generators/pre_render_scripts/generator.sh
@@ -4,7 +4,9 @@ set -o errexit -o nounset
 
 touch "${OUTPUT}"
 
-echo "- type: replace" >> "${OUTPUT}"
-echo "  path: /instance_groups/name=${INSTANCE_GROUP}/jobs/name=${JOB}/properties/bosh_containerization?/pre_render_scripts/-" >> "${OUTPUT}"
-echo "  value: |" >> "${OUTPUT}"
-cat "${PRE_RENDER_SCRIPT}" | sed 's/^/    /' >> "${OUTPUT}"
+{
+  echo "- type: replace";
+  echo "  path: /instance_groups/name=${INSTANCE_GROUP}/jobs/name=${JOB}/properties/bosh_containerization?/pre_render_scripts/-";
+  echo "  value: |";
+  sed 's/^/    /' < "${PRE_RENDER_SCRIPT}";
+} >> "${OUTPUT}"
