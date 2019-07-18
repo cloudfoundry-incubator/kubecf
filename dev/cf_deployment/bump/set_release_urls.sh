1842354a56b2d3311115b3b87dd99b66d854e695
Thulio Ferraz Assis <tassis@suse.com>
2019-07-18 09:32:28 -0700

    Fixed linting issues on set_release_url.sh

--- dev/cf_deployment/bump/set_release_urls.sh
+++ dev/cf_deployment/bump/set_release_urls.sh
@@ -5,12 +5,12 @@ set -o errexit -o nounset
 cf_deployment_yaml="deploy/helm/scf/assets/cf-deployment.yml"
 dest="deploy/helm/scf/assets/operations/set_release_urls.yaml"
 
-printf "# This ops file sets the Quarks images for the releases.\n\n" > "${dest}"
+printf "# This ops file sets the Quarks images for the releases.\\n\\n" > "${dest}"
 
 yq -r '.releases[].name' "${cf_deployment_yaml}" \
   | xargs \
     --no-run-if-empty \
     --replace='[release]' \
-    printf -- "- type: replace\n  path: /releases/name=[release]/url\n  value: docker.io/cfcontainerization\n- type: remove\n  path: /releases/name=[release]/sha1\n\n" \
+    printf -- "- type: replace\\n  path: /releases/name=[release]/url\\n  value: docker.io/cfcontainerization\\n- type: remove\\n  path: /releases/name=[release]/sha1\\n\\n" \
   | head -c -1 \
   >> "${dest}"
