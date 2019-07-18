7ecfdc099eb475520f8a9f9c3e50ad9f4cdb16c6
Thulio Ferraz Assis <tassis@suse.com>
2019-07-18 14:53:24 -0700

    Improved generator.sh based on review comment

--- bosh/releases/generators/pre_render_scripts/generator.sh
+++ bosh/releases/generators/pre_render_scripts/generator.sh
@@ -2,11 +2,11 @@
 
 set -o errexit -o nounset
 
-touch "${OUTPUT}"
-
 {
-  echo "- type: replace";
-  echo "  path: /instance_groups/name=${INSTANCE_GROUP}/jobs/name=${JOB}/properties/bosh_containerization?/pre_render_scripts/-";
-  echo "  value: |";
-  sed 's/^/    /' < "${PRE_RENDER_SCRIPT}";
-} >> "${OUTPUT}"
+cat <<EOT
+- type: replace
+  path: /instance_groups/name=${INSTANCE_GROUP}/jobs/name=${JOB}/properties/bosh_containerization?/pre_render_scripts/-
+  value: |
+EOT
+sed 's/^/    /' < "${PRE_RENDER_SCRIPT}"
+} > "${OUTPUT}"
