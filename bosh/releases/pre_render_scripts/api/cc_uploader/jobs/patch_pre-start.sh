d2397ff91db9860355126553dcd503f2f53746c0
Thulio Ferraz Assis <tassis@suse.com>
2019-07-29 16:15:37 -0700

    Added comment to patch with better reasoning

--- bosh/releases/pre_render_scripts/api/cc_uploader/jobs/patch_pre-start.sh
+++ bosh/releases/pre_render_scripts/api/cc_uploader/jobs/patch_pre-start.sh
@@ -5,6 +5,8 @@ set -o errexit -o nounset
 target="/var/vcap/all-releases/jobs-src/capi/cc_uploader/templates/pre-start.erb"
 
 # Remove sysctl calls as we are running in containers.
+# cc_uploader_ctl in https://github.com/cloudfoundry/capi-release/blob/master/jobs/cc_uploader/templates/cc_uploader_ctl.erb#L26
+# also skips setting those parameters.
 patch --verbose "${target}" <<'EOT'
 @@ -6,6 +6,3 @@
      /var/vcap/jobs/bosh-dns/bin/wait
