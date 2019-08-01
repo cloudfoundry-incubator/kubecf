5c990a1f84d41b5c661423c47244267cb144356b
viovanov <viovanov@suse.com>
2019-08-01 20:39:19 +0300

    Configure the diego-cell rep

---
+++ bosh/releases/pre_render_scripts/diego-cell/cflinuxfs3-rootfs-setup/patch_pre-start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset
+
+target="/var/vcap/all-releases/jobs-src/cflinuxfs3/cflinuxfs3-rootfs-setup/templates/pre-start"
+
+# Remove sysctl calls as we are running in containers.
+patch --verbose "${target}" <<'EOT'
+@@ -3,8 +3,8 @@
+
+ CONF_DIR=/var/vcap/jobs/cflinuxfs3-rootfs-setup/config
+ ROOTFS_PACKAGE=/var/vcap/packages/cflinuxfs3
+-ROOTFS_DIR=$ROOTFS_PACKAGE/rootfs
+-ROOTFS_TAR=$ROOTFS_PACKAGE/rootfs.tar
++ROOTFS_DIR=/var/vcap/data/rep/cflinuxfs3/rootfs
++ROOTFS_TAR=/var/vcap/data/rep/cflinuxfs3/rootfs.tar
+ TRUSTED_CERT_FILE=$CONF_DIR/certs/trusted_ca.crt
+ CA_DIR=$ROOTFS_DIR/usr/local/share/ca-certificates/
+EOT
