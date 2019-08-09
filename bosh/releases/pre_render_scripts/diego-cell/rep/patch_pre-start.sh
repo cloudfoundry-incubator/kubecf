cd9ff6f9e6e58fb4f5aa14e7dd0da1ceb543a702
Thulio Ferraz Assis <tassis@suse.com>
2019-08-09 13:24:25 -0700

    Fixed some review comments

--- bosh/releases/pre_render_scripts/diego-cell/rep/patch_pre-start.sh
+++ bosh/releases/pre_render_scripts/diego-cell/rep/patch_pre-start.sh
@@ -4,11 +4,11 @@ set -o errexit -o nounset
 
 target="/var/vcap/all-releases/jobs-src/diego/rep/templates/bpm-pre-start.erb"
 
-# Use the ephemeral data directory for the rootfs
+# Use the ephemeral data directory for the rootfs.
 patch --verbose "${target}" <<'EOT'
 @@ -5,3 +5,7 @@
  $bin_dir/set-rep-kernel-params
- 
+
  $bin_dir/setup_mounted_data_dirs
 +
 +mkdir -p /var/vcap/data/shared-packages/
