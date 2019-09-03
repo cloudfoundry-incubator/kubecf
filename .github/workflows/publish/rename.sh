9476c7328455cdf6021704ca37e0c2a39bb87d78
Thulio Ferraz Assis <tassis@suse.com>
2019-09-03 15:40:06 -0700

    Added glob scf-*.tgz to ls

--- .github/workflows/publish/rename.sh
+++ .github/workflows/publish/rename.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset
 
-built_file="$(ls output/)"
+built_file="$(ls output/scf-*.tgz)"
 commit_hash="$(git rev-parse --short HEAD)"
 release_filename="$(basename "${built_file}" .tgz)-${commit_hash}"
 if [ ! -z "$(git status --porcelain)" ]; then
