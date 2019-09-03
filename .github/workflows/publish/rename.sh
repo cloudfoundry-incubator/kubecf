5d2379efb8b87398c3a3d43302867246842a3c75
Thulio Ferraz Assis <tassis@suse.com>
2019-09-03 15:42:02 -0700

    Better logic

--- .github/workflows/publish/rename.sh
+++ .github/workflows/publish/rename.sh
@@ -5,7 +5,7 @@ set -o errexit -o nounset
 built_file="$(ls output/scf-*.tgz)"
 commit_hash="$(git rev-parse --short HEAD)"
 release_filename="$(basename "${built_file}" .tgz)-${commit_hash}"
-if [ ! -z "$(git status --porcelain)" ]; then
+if [ -n "$(git status --porcelain)" ]; then
   release_filename="${release_filename}-dirty"
 fi
 
