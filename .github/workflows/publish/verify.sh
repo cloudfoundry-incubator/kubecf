36b7b622c1b229386d8beaf263e787647e4962e4
Thulio Ferraz Assis <tassis@suse.com>
2019-09-04 01:11:56 -0700

    Fixed verify step on v3 publish pipeline

--- .github/workflows/publish/verify.sh
+++ .github/workflows/publish/verify.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset
 
-if [[ "$(ls -a output | wc -l)" != 1 ]]; then exit 1; fi
+if [[ "$(ls output/scf-*.tgz | wc -l)" != 1 ]]; then exit 1; fi
 
 ls -lha output/scf-*.tgz
 tar tvf output/scf-*.tgz
