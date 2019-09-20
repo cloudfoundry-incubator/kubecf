5c45a57a8ec87c81910b6246a1b04ee907064012
Thulio Ferraz Assis <tassis@suse.com>
2019-09-20 11:03:26 -0700

    Fixed linting issues

--- .github/workflows/publish/verify.sh
+++ .github/workflows/publish/verify.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset
 
-if [[ "$(ls output/scf-*.tgz | wc -l)" != 1 ]]; then exit 1; fi
+if [[ "$(find output/scf-*.tgz | wc -l)" != 1 ]]; then exit 1; fi
 
 ls -lha output/scf-*.tgz
 tar tvf output/scf-*.tgz
