15f787085ae9c7d32bf3227d020fc91a31c7cbd2
Thulio Ferraz Assis <tassis@suse.com>
2019-09-20 06:58:37 -0700

    Added Gitlab runner installation for Minikube

---
+++ .gitlab/deploy/runner/kubernetes/minikube/README.md
@@ -0,0 +1,4 @@
+# Deploy Gitlab runner on Minikube
+
+Run the `deploy` target to install the Gitlab runner using Helm. This target assumes Helm Tiller is
+already installed.
---
+++ .gitlab/deploy/runner/kubernetes/minikube/deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset
+
+default_installation_name="gitlab-runner"
+printf "Installation name (%s): " "${default_installation_name}"
+read -r installation_name
+if [ -z "${installation_name}" ]; then
+  installation_name="${default_installation_name}"
+fi
+
+default_namespace="gitlab-runner"
+printf "Namespace (%s): " "${default_namespace}"
+read -r namespace
+if [ -z "${namespace}" ]; then
+  namespace="${default_namespace}"
+fi
+
+default_personal_id="$(whoami)"
+printf "Personal ID (%s): " "${default_personal_id}"
+read -r personal_id
+if [ -z "${personal_id}" ]; then
+  personal_id="${default_personal_id}"
+fi
+
+stty -echo
+printf "Runner registration token: "
+read -r runner_registration_token
+stty echo
+printf "\\n"
+if [ -z "${runner_registration_token}" ]; then
+  >&2 echo "The runner registration token cannot be empty"
+  exit 1
+fi
+
+helm upgrade "${installation_name}" gitlab/gitlab-runner \
+  --install \
+  --namespace "${namespace}" \
+  --values .gitlab/deploy/runner/kubernetes/minikube/values.yaml \
+  --set "runners.namespace=${namespace}" \
+  --set "runners.tags=personal-minikube-${default_personal_id}" \
+  --set "runnerRegistrationToken=${runner_registration_token}"
