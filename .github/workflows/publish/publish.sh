d43c9364b828245b982787676b625c38d2bc2fc6
Thulio Ferraz Assis <tassis@suse.com>
2019-09-23 11:41:32 -0700

    Removed GitHub Actions

--- .github/workflows/publish/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -o errexit -o nounset
-
-alias="s3"
-
-mc config host add "${alias}" "https://s3.amazonaws.com" "${AWS_ACCESS_KEY}" "${AWS_SECRET_KEY}"
-mc cp output/scf-*.tgz "${alias}/${BUCKET}"
-echo "URL: https://${BUCKET}.s3.amazonaws.com/$(find output/ -name 'scf-*.tgz' -print0 | xargs -0 basename)"
--- .github/workflows/publish/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit -o nounset
-
-if [[ "$(find output/scf-*.tgz | wc -l)" != 1 ]]; then exit 1; fi
-
-ls -lha output/scf-*.tgz
-tar tvf output/scf-*.tgz
