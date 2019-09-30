f6e607d4a81da6bf5b92e8b0f7221a219e1704ef
Jakob Schmid <jakob.schmid@sap.com>
2019-09-30 09:28:37 +0200

    Make patch for eirini-loggregator-bridge bpm idempotent
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>

--- bosh/releases/pre_render_scripts/eirini/eirini-loggregator-bridge/bpm/patch_bpm.sh
+++ bosh/releases/pre_render_scripts/eirini/eirini-loggregator-bridge/bpm/patch_bpm.sh
@@ -5,20 +5,28 @@ set -o errexit -o nounset
 target="/var/vcap/all-releases/jobs-src/eirini/eirini-loggregator-bridge/templates/bpm.yml.erb"
 
 # Patch BPM, since we're actually running in-cluster without BPM
-patch --verbose "${target}" <<'EOT'
-11,24d10
-<     env:
-<       KUBERNETES_SERVICE_HOST: "<%= p("eirini-loggregator-bridge.kube_service_host") %>"
-<       KUBERNETES_SERVICE_PORT: "<%= p("eirini-loggregator-bridge.kube_service_port") %>"
-<     <% if properties.opi&.k8s&.host_url.nil? %>
-<     # The ServiceAccount admission controller has to be enabled.
-<     # https://kubernetes.io/docs/tasks/access-application-cluster/access-cluster/#accessing-the-api-from-a-pod
-<     additional_volumes:
-<     - path: /var/run/secrets/kubernetes.io/serviceaccount/token
-<       mount_only: true
-<     - path: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-<       mount_only: true
-<     - path: /var/run/secrets/kubernetes.io/serviceaccount/namespace
-<       mount_only: true
-<     <% end %>
-EOT
+PATCH='@@ -8,17 +8,3 @@
+       - "--kubeconfig"
+       - "<%= kubeconfig %>"
+       <% end %>
+-    env:
+-      KUBERNETES_SERVICE_HOST: "<%= p("eirini-loggregator-bridge.kube_service_host") %>"
+-      KUBERNETES_SERVICE_PORT: "<%= p("eirini-loggregator-bridge.kube_service_port") %>"
+-    <% if properties.opi&.k8s&.host_url.nil? %>
+-    # The ServiceAccount admission controller has to be enabled.
+-    # https://kubernetes.io/docs/tasks/access-application-cluster/access-cluster/#accessing-the-api-from-a-pod
+-    additional_volumes:
+-    - path: /var/run/secrets/kubernetes.io/serviceaccount/token
+-      mount_only: true
+-    - path: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+-      mount_only: true
+-    - path: /var/run/secrets/kubernetes.io/serviceaccount/namespace
+-      mount_only: true
+-    <% end %>'
+
+# Only patch once
+if ! patch --reverse --dry-run -f "${target}" <<<"$PATCH" 2>&1  >/dev/null ; then
+  patch --verbose "${target}" <<<"$PATCH"
+else
+  echo "Patch already applied. skipping"
+fi
