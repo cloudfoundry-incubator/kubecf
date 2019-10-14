90512301ddd5bf4a9b483104d1f1b4054ffb3019
Ralf Pannemans <ralf.pannemans@sap.com>
2019-10-14 14:35:22 +0200

    Use unified diff for patches with deletes only.
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>

--- bosh/releases/pre_render_scripts/api/cloud_controller_ng/bpm/patch_bpm.sh
+++ bosh/releases/pre_render_scripts/api/cloud_controller_ng/bpm/patch_bpm.sh
@@ -9,11 +9,23 @@ target="/var/vcap/all-releases/jobs-src/capi/cloud_controller_ng/templates/bpm.y
 #   - We don't enable New Relic.
 #   - NGINX maintenance shouldn't run.
 PATCH=$(cat <<'EOT'
-23d22
-<     "DYNO" => "#{spec.job.name}-#{spec.index}",
-77,78d75
-<     nginx_newrelic_plugin_config,
-<     nginx_maintenance_config,
+@@ -20,7 +20,6 @@
+     "BUNDLE_GEMFILE" => "/var/vcap/packages/cloud_controller_ng/cloud_controller_ng/Gemfile",
+     "CLOUD_CONTROLLER_NG_CONFIG" => "/var/vcap/jobs/cloud_controller_ng/config/cloud_controller_ng.yml",
+     "C_INCLUDE_PATH" => "/var/vcap/packages/libpq/include",
+-    "DYNO" => "#{spec.job.name}-#{spec.index}",
+     "HOME" => "/home/vcap",
+     "LANG" => "en_US.UTF-8",
+     "LIBRARY_PATH" => "/var/vcap/packages/libpq/lib",
+@@ -79,8 +78,6 @@
+   "processes" => [
+     cloud_controller_ng_config,
+     nginx_config,
+-    nginx_newrelic_plugin_config,
+-    nginx_maintenance_config,
+     ccng_monit_http_healthcheck_config,
+   ]
+ }
 EOT
 )
 
--- bosh/releases/pre_render_scripts/eirini/opi/bpm/patch_bpm.sh
+++ bosh/releases/pre_render_scripts/eirini/opi/bpm/patch_bpm.sh
@@ -6,21 +6,24 @@ target="/var/vcap/all-releases/jobs-src/eirini/opi/templates/bpm.yml.erb"
 
 # Patch BPM, since we're actually running in-cluster without BPM
 PATCH=$(cat <<'EOT'
-7,20d6
-<     env:
-<       KUBERNETES_SERVICE_HOST: "<%= p("opi.kube_service_host") %>"
-<       KUBERNETES_SERVICE_PORT: "<%= p("opi.kube_service_port") %>"
-<     <% if properties.opi&.k8s&.host_url.nil? %>
-<     # The ServiceAccount admission controller has to be enabled.
-<     # https://kubernetes.io/docs/tasks/access-application-cluster/access-cluster/#accessing-the-api-from-a-pod
-<     additional_volumes:
-<     - path: /var/run/secrets/kubernetes.io/serviceaccount/token
-<       mount_only: true
-<     - path: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-<       mount_only: true
-<     - path: /var/run/secrets/kubernetes.io/serviceaccount/namespace
-<       mount_only: true
-<     <% end %>
+@@ -4,17 +4,3 @@
+     args:
+     - connect
+     - --config=/var/vcap/jobs/opi/config/opi.yml
+-    env:
+-      KUBERNETES_SERVICE_HOST: "<%= p("opi.kube_service_host") %>"
+-      KUBERNETES_SERVICE_PORT: "<%= p("opi.kube_service_port") %>"
+-    <% if properties.opi&.k8s&.host_url.nil? %>
+-    # The ServiceAccount admission controller has to be enabled.
+-    # https://kubernetes.io/docs/tasks/access-application-cluster/access-cluster/#accessing-the-api-from-a-pod
+-    additional_volumes:
+-    - path: /var/run/secrets/kubernetes.io/serviceaccount/token
+-      mount_only: true
+-    - path: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+-      mount_only: true
+-    - path: /var/run/secrets/kubernetes.io/serviceaccount/namespace
+-      mount_only: true
+-    <% end %>
 EOT
 )
 
