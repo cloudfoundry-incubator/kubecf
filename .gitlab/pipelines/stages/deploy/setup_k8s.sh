a3dbd9c8986b4ebf5bf9170d9cc816ab7445e666
Thulio Ferraz Assis <tassis@suse.com>
2019-11-01 16:51:57 -0700

    Fixed review comments

--- .gitlab/pipelines/stages/deploy/setup_k8s.sh
+++ .gitlab/pipelines/stages/deploy/setup_k8s.sh
@@ -8,24 +8,34 @@ source "$(bazel info workspace)/.gitlab/pipelines/config/config.sh"
 wait_for_file() {
   local file_path="$1"
   local timeout="${2:-30}"
-  until [[ -f "${file_path}" ]] || [[ "$timeout" == "0" ]]; do sleep 1; timeout=$((timeout - 1)); done
-  if [[ "${timeout}" == 0 ]]; then return 1; fi
-  return 0
+  for (( ; timeout > 0 ; timeout -- )); do
+    if [[ -f "${file_path}" ]]; then
+      return 0
+    fi
+    sleep 1
+  done
+  return 1
 }
 
 printf "k3s: %s\n" "$(bazel run @k3s//executable -- --version 2> /dev/null)"
-sudo -E "$(command -v bazel)" run @k3s//executable -- server 1> /tmp/k3s.out.log 2> /tmp/k3s.err.log &
+sudo -E "$(command -v bazel)" run @k3s//executable -- server --log /dev/null &
 k3s_kubeconfig="/etc/rancher/k3s/k3s.yaml"
 kubeconfig="${HOME}/.kube/config"
-wait_for_file "${k3s_kubeconfig}" || {
+if ! wait_for_file "${k3s_kubeconfig}"; then
   >&2 echo "${k3s_kubeconfig} did not get created"
-}
+fi
 mkdir -p "$(dirname "${kubeconfig}")"
-sudo cp "${k3s_kubeconfig}" "${kubeconfig}"
-sudo chown "$(id -u)":"$(id -g)" "${kubeconfig}"
+(sudo cat "${k3s_kubeconfig}") > "${kubeconfig}"
 
 bazel run //dev/kube/local_path_provisioner:apply
 bazel run @kubectl//:kubectl -- patch storageclass local-path \
-  --patch '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true", "storageclass.beta.kubernetes.io/is-default-class":"true"}}}'
+  --patch '{
+  "metadata": {
+    "annotations": {
+      "storageclass.kubernetes.io/is-default-class": "true",
+      "storageclass.beta.kubernetes.io/is-default-class": "true"
+    }
+  }
+}'
 
 bazel run @kubectl//:kubectl -- create namespace "${CF_OPERATOR_NAMESPACE}"
--- .gitlab/pipelines/stages/deploy/wait_kubecf.sh
+++ .gitlab/pipelines/stages/deploy/wait_kubecf.sh
@@ -5,10 +5,16 @@ set -o errexit -o nounset
 # shellcheck disable=SC1090
 source "$(bazel info workspace)/.gitlab/pipelines/config/config.sh"
 
+get_endpoint() {
+  bazel run @kubectl//:kubectl -- get endpoint kubecf-router-public \
+    --namespace "${KUBECF_NAMESPACE}" \
+    --output jsonpath='{.subsets[0].addresses[0].ip}' 2> /dev/null
+}
+
 # Wait for kubecf to start.
 wait_for_router() {
   local timeout="1800"
-  until [[ "$(bazel run @kubectl//:kubectl -- get endpoints --namespace "${KUBECF_NAMESPACE}" kubecf-router-public --output jsonpath='{.subsets[0].addresses[0].ip}' 2> /dev/null)" != "" ]] || [[ "$timeout" == "0" ]]; do sleep 1; timeout=$((timeout - 1)); done
+  until [[ "$(get_endpoint)" != "" ]] || [[ "$timeout" == "0" ]]; do sleep 1; timeout=$((timeout - 1)); done
   if [[ "${timeout}" == 0 ]]; then return 1; fi
   return 0
 }
