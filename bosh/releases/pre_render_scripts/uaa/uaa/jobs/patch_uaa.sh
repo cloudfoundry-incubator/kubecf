84e595e9f69011e57e3ed81ddfd97dac28a7c1a1
Matthias Diester <matthias.diester@de.ibm.com>
2019-11-06 22:30:48 +0100

    Add OS switch in UAA start script patch
    Add operating system switch into the UAA start script using the existing
    patch for that file. Depending on the operating system, different system
    paths will be used.

--- bosh/releases/pre_render_scripts/uaa/uaa/jobs/patch_uaa.sh
+++ bosh/releases/pre_render_scripts/uaa/uaa/jobs/patch_uaa.sh
@@ -6,10 +6,32 @@ target="/var/vcap/all-releases/jobs-src/uaa/uaa/templates/bin/uaa.erb"
 
 # Patch bin/uaa.erb for the certificates to work with SUSE.
 PATCH=$(cat <<'EOT'
-49c49
-< cp /etc/ssl/certs/ca-certificates.crt "$CERT_FILE"
----
-> cp /var/lib/ca-certificates/ca-bundle.pem "$CERT_FILE"
+--- uaa.erb    2019-11-06 21:27:49.000000000 +0100
++++ -     2019-11-06 21:42:06.000000000 +0100
+@@ -46,7 +46,22 @@
+
+ echo "`date` [uaa] UAA Preparing Certs"
+ CERT_FILE="/tmp/ca-certificates.crt"
+-cp /etc/ssl/certs/ca-certificates.crt "$CERT_FILE"
++
++source /etc/os-release
++case "${ID}" in
++  *ubuntu*)
++    cp /etc/ssl/certs/ca-certificates.crt "$CERT_FILE"
++    ;;
++
++  *suse*)
++    cp /var/lib/ca-certificates/ca-bundle.pem "$CERT_FILE"
++    ;;
++
++  *)
++    echo "Unsupported operating system: ${PRETTY_NAME}"
++    exit 42
++    ;;
++esac
+
+ CONF_DIR="/var/vcap/jobs/uaa/config"
+ CACHE_DIR="/var/vcap/data/uaa/cert-cache"
 EOT
 )
 
