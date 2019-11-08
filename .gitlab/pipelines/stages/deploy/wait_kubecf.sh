0528d44315994353840ef9d0cfa71568d544e74c
Thulio Ferraz Assis <tassis@suse.com>
2019-11-08 12:39:49 -0800

    Bumped cf-operator that includes update serial

--- .gitlab/pipelines/stages/deploy/wait_kubecf.sh
+++ .gitlab/pipelines/stages/deploy/wait_kubecf.sh
@@ -7,26 +7,28 @@ source "$(bazel info workspace)/.gitlab/pipelines/runtime/config.sh"
 # shellcheck disable=SC1090
 source "$(bazel info workspace)/.gitlab/pipelines/runtime/binaries.sh"
 
-get_endpoint() {
-  "${KUBECTL}" get endpoints kubecf-router-public \
-    --namespace "${KUBECF_NAMESPACE}" \
-    --output jsonpath='{.subsets[0].addresses[0].ip}' 2> /dev/null
+wait_kubecf() {
+  timeout 1800 bash -c "
+    pod_count() {
+      '${KUBECTL}' get pods --namespace ${KUBECF_NAMESPACE} --no-headers \
+        | wc -l
+    }
+    ready_pod_count() {
+      '${KUBECTL}' get pods --namespace ${KUBECF_NAMESPACE} --no-headers \
+        | awk '{ printf \"%s-%s\n\", \$2, \$3 }' | grep -x '\(.*\)\/\1-Running' \
+        | wc -l
+    }
+    while true; do
+      if [[ \"\$(pod_count)\" != \"0\" ]] && [[ \"\$(pod_count)\" == \"\$(ready_pod_count)\" ]]; then
+        exit 0
+      fi
+      sleep 1
+    done
+  "
 }
 
-# Wait for kubecf to start.
-wait_for_router() {
-  local timeout
-  for (( timeout = 1800; timeout > 0; timeout -- )); do
-    if [[ "$(get_endpoint)" != "" ]]; then
-      return 0
-    fi
-    sleep 1
-  done
-  return 1
-}
-
-echo "Waiting for the router pod to become available..."
-wait_for_router || {
-  >&2 echo "Timed out waiting for the router pod"
+echo "Waiting for kubecf to be ready..."
+wait_kubecf || {
+  >&2 echo "Timed out waiting for kubecf to be ready"
   exit 1
 }
