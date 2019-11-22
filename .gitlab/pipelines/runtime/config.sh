958fe9548e108ca878caa3c3c69b85318e3352fb
Thulio Ferraz Assis <tassis@suse.com>
2019-11-22 14:41:54 -0800

    Updated CI for the ssh-proxy

--- .gitlab/pipelines/runtime/config.sh
+++ .gitlab/pipelines/runtime/config.sh
@@ -2,8 +2,9 @@
 
 export KUBECF_NAMESPACE="kubecf"
 export CF_OPERATOR_NAMESPACE="cfo"
-export CLUSTER_IP="10.43.0.255"
-export SYSTEM_DOMAIN="${CLUSTER_IP}.nip.io"
+export ROUTER_CLUSTER_IP="10.43.0.250"
+export SSH_PROXY_CLUSTER_IP="10.43.0.251"
+export SYSTEM_DOMAIN="kubecf-ci.susecap.net"
 : "${EIRINI_ENABLED:=false}"
 export EIRINI_ENABLED
 export KUBECF_CHART_TARGET="//deploy/helm/kubecf:chart"
