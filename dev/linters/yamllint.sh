07867dff65b301bb58656d02d4e0af0fe8666af0
Thulio Ferraz Assis <tassis@suse.com>
2019-11-27 19:25:06 -0800

    Fixed yamllint script to use absolute path to workspace

--- dev/linters/yamllint.sh
+++ dev/linters/yamllint.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-set -o errexit
+set -o errexit -o nounset
+
+workspace=$(bazel info workspace)
 
 # shellcheck disable=SC2046
 # We want word splitting with find.
 bazel run //dev/linters/yamllint -- \
       -d "{extends: relaxed, rules: {line-length: {max: 120}}}" \
-      --strict $(find . -type f \
-                      -path "./deploy/helm/kubecf/values.*" \
-                      -or -not -path "./deploy/helm/kubecf/*" \
+      --strict $(find "${workspace}" -type f \
+                      -path "${workspace}/deploy/helm/kubecf/values.*" \
+                      -or -not -path "${workspace}/deploy/helm/kubecf/*" \
                       -name '*.yaml' -or -name '*.yml')
