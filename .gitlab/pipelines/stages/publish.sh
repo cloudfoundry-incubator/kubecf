cb180aa56f8c159344d7888cb62c3976dae57d0d
Mark Yen <mark.yen@suse.com>
2019-12-04 14:34:40 -0800

    Simplify publishing (do not publish the bundle)
    The bundle (kubecf + operator) doesn't need to be published right now;
    we can do so if somebody actually asks for it.  In the mean time, we
    still upload them to CI, so people can get them there.

--- .gitlab/pipelines/stages/publish.sh
+++ .gitlab/pipelines/stages/publish.sh
@@ -3,7 +3,5 @@
 set -o errexit -o nounset
 
 mc config host add "s3" "https://s3.amazonaws.com" "${AWS_ACCESS_KEY}" "${AWS_SECRET_KEY}"
-for file in output/kubecf-*.tgz ; do
-    mc cp "${file}" "s3/scf-v3"
-    echo "URL: https://scf-v3.s3.amazonaws.com/$(basename "${file}")"
-done
+mc cp output/kubecf-*.tgz "s3/scf-v3"
+echo "URL: https://scf-v3.s3.amazonaws.com/$(basename output/kubecf-*.tgz)"
