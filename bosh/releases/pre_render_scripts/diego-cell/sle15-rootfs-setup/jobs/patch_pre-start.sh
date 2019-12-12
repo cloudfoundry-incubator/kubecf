2bd565f1641dcd108dd00a8ae69e2813064e3fdf
Bikramdeep Singh <bikramdeep.singh@suse.com>
2019-12-12 14:38:17 -0800

    implementing review comments

--- bosh/releases/pre_render_scripts/diego-cell/sle15-rootfs-setup/jobs/patch_pre-start.sh
+++ bosh/releases/pre_render_scripts/diego-cell/sle15-rootfs-setup/jobs/patch_pre-start.sh
@@ -3,11 +3,15 @@
 set -o errexit -o nounset
 
 target="/var/vcap/all-releases/jobs-src/sle15/sle15-rootfs-setup/templates/pre-start"
+sentinel="${target}.patch_sentinel"
+if [[ -f "${sentinel}" ]]; then
+  echo "Patch already applied. Skipping"
+  exit 0
+fi
 
 # Use the ephemeral data directory for the rootfs
-PATCH=$(cat <<'EOT'
+patch --verbose "${target}" <<'EOT'
 @@ -3,8 +3,8 @@
- 
  CONF_DIR=/var/vcap/jobs/sle15-rootfs-setup/config
  ROOTFS_PACKAGE=/var/vcap/packages/sle15
 -ROOTFS_DIR=$ROOTFS_PACKAGE/rootfs
@@ -15,11 +19,5 @@ PATCH=$(cat <<'EOT'
 +ROOTFS_DIR=/var/vcap/data/rep/sle15/rootfs
 +ROOTFS_TAR=/var/vcap/data/rep/sle15/rootfs.tar
 EOT
-)
 
-# Only patch once
-if ! patch --reverse --dry-run -f "${target}" <<<"$PATCH" 2>&1  >/dev/null ; then
-  patch --verbose "${target}" <<<"$PATCH"
-else
-  echo "Patch already applied. Skipping"
-fi
+touch "${sentinel}"
