862decfe69922d2985db5dc5c82a23d548506d96
Thulio Ferraz Assis <tassis@suse.com>
2019-12-16 16:44:52 -0800

    Mapped DRONE_DATABASE_DATASOURCE to DATABASE_URL

--- .drone/deploy/server/image/run.sh
+++ .drone/deploy/server/image/run.sh
@@ -11,6 +11,7 @@ convert_starlark_secret="dont_need_to_be_secret"
 DRONE_SECRET="${convert_starlark_secret}" exec /bin/drone-convert-starlark &
 
 export DRONE_SERVER_PORT=":${PORT}"
+export DRONE_DATABASE_DATASOURCE="${DATABASE_URL}"
 export DRONE_CONVERT_PLUGIN_ENDPOINT=http://127.0.0.1:3000
 DRONE_CONVERT_PLUGIN_SECRET="${convert_starlark_secret}"
 export DRONE_CONVERT_PLUGIN_SECRET
