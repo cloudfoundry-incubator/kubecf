39ca7f479a670b00037ec560334040c26b0a4b62
Andreas Kupries <andreas.kupries@suse.com>
2020-01-20 09:33:59 -0800

    Moved from find + grep (to filter) to `git ls-files` to use .gitignore in full.

--- dev/linters/shellcheck.sh
+++ dev/linters/shellcheck.sh
@@ -4,7 +4,9 @@ set -o errexit -o nounset
 
 workspace=$(bazel info workspace)
 
+# Using git ls-files ensures that .gitignored files are skipped.
+# sed: We need absolute paths for bazel
+# We want work splitting with ls-files.
 # shellcheck disable=SC2046
-# We want word splitting with find.
 bazel run @shellcheck//:binary -- \
-      $(find "${workspace}" -name '*.sh' | grep -v '/_work/')
+      $(git ls-files | awk '/.sh$/ { print $0 }' | sed -e "s|^|${workspace}/|")
