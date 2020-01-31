44910fc5167cd3620107b0643702504d7f2990f5
Mark Yen <mark.yen@suse.com>
2020-01-31 14:20:48 -0800

    feat: containers: push all images as one bundle
    This makes it easier to have a single target that should push all the
    docker images.

--- workspace_status.rb
+++ workspace_status.rb
@@ -7,5 +7,6 @@ git_commit_short = `git rev-parse --short --verify HEAD`.strip!
 
 puts "STABLE_GIT_BRANCH #{git_branch}"
 puts "STABLE_GIT_COMMIT_SHORT #{git_commit_short}"
+puts "STABLE_DOCKER_ORG #{ENV['KUBECF_DOCKER_ORG'] || 'cfcontainerization'}"
 
 require_relative 'deploy/containers/credhub_setup/workspace_status'
