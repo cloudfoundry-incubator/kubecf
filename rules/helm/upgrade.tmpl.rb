885b2a877dfcbdcb54c5f52a482d0772cba8c75c
Thulio Ferraz Assis <tassis@suse.com>
2020-02-07 14:54:16 -0800

    feat: added the helm upgrade rule

---
+++ rules/helm/upgrade.tmpl.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require 'json'
+require 'open3'
+
+# Variable interpolation via Bazel template expansion.
+helm = '[[helm]]'
+install_name = '[[install_name]]'
+chart_package = '[[chart_package]]'
+namespace = '[[namespace]]'
+install = '[[install]]' == 'True'
+reuse_values = '[[reuse_values]]' == 'True'
+values_paths = '[[values_paths]]'
+path_split_delim = '[[path_split_delim]]'
+set_values = JSON.parse('[[set_values]]')
+
+args = [
+  helm, 'upgrade', install_name, chart_package,
+  '--namespace', namespace
+]
+
+args.append('--install') if install
+args.append('--reuse-values') if reuse_values
+
+values = values_paths.split(path_split_delim).map do |path|
+  ['--values', path]
+end
+args.concat(*values)
+
+set_values = set_values.map do |key, value|
+  ['--set', "#{key}=#{value}"]
+end
+args.concat(*set_values)
+
+stdout, stderr, status = Open3.capture3(*args)
+unless status.success?
+  puts stderr
+  exit 1
+end
+
+puts stdout
