477e24a76a73adf9de1b51488bb8946bb2cf3abd
André Duffeck <aduffeck@suse.com>
2020-03-02 14:29:22 +0100

    Fix helm upgrades not applying changed values
    "helm upgrade --reuse-values" reuses the values of the last deployment
    and applies the ones given on the command line, but it discards changed
    values in the chart itself.
    What we actually want is "--reset-values" which resets the values to the
    ones from the chart + command line.

--- rules/helm/upgrade.tmpl.rb
+++ rules/helm/upgrade.tmpl.rb
@@ -10,7 +10,7 @@ install_name = '[[install_name]]'
 chart_package = '[[chart_package]]'
 namespace = '[[namespace]]'
 install = '[[install]]' == 'True'
-reuse_values = '[[reuse_values]]' == 'True'
+reset_values = '[[reset_values]]' == 'True'
 values_paths = '[[values_paths]]'
 path_split_delim = '[[path_split_delim]]'
 set_values = JSON.parse('[[set_values]]')
@@ -21,7 +21,7 @@ args = [
 ]
 
 args.push('--install') if install
-args.push('--reuse-values') if reuse_values
+args.push('--reset-values') if reset_values
 
 values = values_paths.split(path_split_delim).map do |path|
   ['--values', path]
