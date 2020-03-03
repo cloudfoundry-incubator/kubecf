529302a8ac7439e1027ee6e58b77ce5e5f9d8058
André Duffeck <aduffeck@suse.com>
2020-03-03 14:26:58 +0100

    Update database on config changes, e.g. rotated passwords

--- deploy/helm/kubecf/assets/scripts/jobs/pxc/seeder.sh
+++ deploy/helm/kubecf/assets/scripts/jobs/pxc/seeder.sh
@@ -24,9 +24,9 @@ mysql --host="${DATABASE_HOST}" --user=root --password="${DATABASE_ROOT_PASSWORD
   2> /dev/null \
   < <(
     echo "\
-      CREATE DATABASE kubecf;
+      CREATE DATABASE IF NOT EXISTS kubecf;
       USE kubecf;
-      CREATE TABLE db_leader_election (
+      CREATE TABLE IF NOT EXISTS db_leader_election (
         anchor tinyint(3) unsigned NOT NULL,
         host varchar(128) NOT NULL,
         last_seen_active timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -35,7 +35,8 @@ mysql --host="${DATABASE_HOST}" --user=root --password="${DATABASE_ROOT_PASSWORD
     for database in ${databases[*]}; do
       password=$(</passwords/"${database}"/password)
 
-      echo "CREATE USER \`${database}\` IDENTIFIED BY '${password}';"
+      echo "CREATE USER IF NOT EXISTS \`${database}\`;"
+      echo "ALTER USER \`${database}\` IDENTIFIED BY '${password}';"
       echo "CREATE DATABASE IF NOT EXISTS \`${database}\`;"
       echo "GRANT ALL ON \`${database}\`.* TO '${database}'@'%';"
     done
