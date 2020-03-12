988323b2a9036d611bfb3ef6469d720d0b36f5ff
Bikramdeep Singh <bikramdeep.singh@suse.com>
2020-03-12 11:11:40 -0700

    doc: added comments for the fix

--- deploy/helm/kubecf/assets/scripts/jobs/postgres/bpm_process.sh
+++ deploy/helm/kubecf/assets/scripts/jobs/postgres/bpm_process.sh
@@ -12,6 +12,8 @@ wait_for_file() {
 # shellcheck disable=SC1091
 source /var/vcap/jobs/postgres/bin/pgconfig.sh
 
+# fixes permissions issue for autoscaler db.
+# https://github.com/cloudfoundry-incubator/kubecf/issues/408
 chmod --recursive 0700 /var/vcap/store/postgres/
 
 /var/vcap/jobs/postgres/bin/postgres_ctl start
