da0e3bd19f83bf58ffaa06ff6972d0ed46930e8e
Thulio Ferraz Assis <tassis@suse.com>
2020-03-16 13:23:13 -0500

    fix: remove global dependencies inside functions

--- deploy/helm/kubecf/assets/scripts/jobs/cf-cli-6-linux/setup_internal_security_group.sh
+++ deploy/helm/kubecf/assets/scripts/jobs/cf-cli-6-linux/setup_internal_security_group.sh
@@ -3,21 +3,11 @@
 # Never use -x or -o xtrace here because of security reasons.
 set -o errexit -o nounset -o pipefail
 
-# Write the CF_API_CA_CERT and UAA_CA_CERT environment variable contents to PEM files.
-if [ "${CF_API_CA_CERT:-}" != "" ]; then
-    cf_api_ca_cert_pem="${DATA_DIR}/cf_api_ca_cert.pem"
-    echo -n "${CF_API_CA_CERT}" > "${cf_api_ca_cert_pem}"
-    uaa_ca_cert_pem="${DATA_DIR}/uaa_ca_cert.pem"
-    echo -n "${UAA_CA_CERT}" > "${uaa_ca_cert_pem}"
-fi
-
 # wait_for_endpoint waits for an endpoint to be ready. It fails if the error is related to the
 # certificate.
 function wait_for_endpoint() {
-    local ca_cert
-    ca_cert="$1"
-    local endpoint
-    endpoint="$2"
+    local ca_cert="$1"
+    local endpoint="$2"
     while true; do
         set +o errexit
         output=$(curl --cacert "${ca_cert}" --fail --head --silent --show-error "${endpoint}" 2>&1)
@@ -34,6 +24,157 @@ function wait_for_endpoint() {
         esac
     done
 }
+
+# get_access_token returns a token to be used on the CF API calls.
+function get_access_token() {
+    local url="$1"
+    local cacert="$2"
+    local client_id="$3"
+    local client_secret="$4"
+    curl \
+        --silent --show-error --fail \
+        --cacert "${cacert}" \
+        --request POST \
+        --data "scope=cloud_controller.admin" \
+        --data "grant_type=client_credentials" \
+        --data "client_id=${client_id}" \
+        --data "client_secret=${client_secret}" \
+        "${url}" \
+        | jq -r '.access_token'
+}
+
+# check_security_group checks if the security group already exists.
+function check_security_group() {
+    local cf_api="$1"
+    local cacert="$2"
+    local access_token="$3"
+    local sec_group_name="$4"
+    local total_results
+    total_results=$(
+        curl \
+            --silent --show-error --fail \
+            --cacert "${cacert}" \
+            --header "Authorization: Bearer ${access_token}" \
+            "${cf_api}/v2/security_groups?q=name:${sec_group_name}" \
+            | jq -r '.total_results'
+    )
+    if [[ "${total_results}" == "0" ]]; then return 1; fi
+}
+
+# get_sec_group_id returns the security group ID.
+function get_sec_group_id() {
+    local cf_api="$1"
+    local cacert="$2"
+    local access_token="$3"
+    local sec_group_name="$4"
+    curl \
+        --silent --show-error --fail \
+        --cacert "${cacert}" \
+        --header "Authorization: Bearer ${access_token}" \
+        "${cf_api}/v2/security_groups?q=name:${sec_group_name}" \
+        | jq -r '.resources[0].metadata.guid'
+}
+
+# create_security_group creates the new security group.
+function create_security_group() {
+    local cf_api="$1"
+    local cacert="$2"
+    local access_token="$3"
+    local sec_group_json="$4"
+    curl \
+        --silent --show-error --fail \
+        --cacert "${cacert}" \
+        --header "Authorization: Bearer ${access_token}" \
+        --header "Content-Type: application/json" \
+        --request POST \
+        --data "${sec_group_json}" \
+         "${cf_api}/v2/security_groups" \
+        1> /dev/null
+}
+
+# update_security_group updates the security group with the up-to-date JSON definition.
+function update_security_group() {
+    local cf_api="$1"
+    local cacert="$2"
+    local access_token="$3"
+    local sec_group_id="$4"
+    local sec_group_json="$5"
+    curl \
+        --silent --show-error --fail \
+        --cacert "${cacert}" \
+        --header "Authorization: Bearer ${access_token}" \
+        --header "Content-Type: application/json" \
+        --request PUT \
+        --data "${sec_group_json}" \
+        "${cf_api}/v2/security_groups/${sec_group_id}" \
+        1> /dev/null
+}
+
+# delete_security_group deletes the security group. It assumes the staging and running configs are
+# unbound.
+function delete_security_group() {
+    local cf_api="$1"
+    local cacert="$2"
+    local access_token="$3"
+    local sec_group_id="$4"
+    curl \
+        --silent --show-error --fail \
+        --cacert "${cacert}" \
+        --header "Authorization: Bearer ${access_token}" \
+        --request DELETE \
+        "${cf_api}/v2/security_groups/${sec_group_id}" \
+        1> /dev/null
+}
+
+# bind_security_group binds the security group to the staging and running configs.
+function bind_security_group() {
+    local cf_api="$1"
+    local cacert="$2"
+    local access_token="$3"
+    local sec_group_id="$4"
+    local configs=(
+        "staging_security_groups"
+        "running_security_groups"
+    )
+    for config in "${configs[@]}"; do
+        curl \
+            --silent --show-error --fail \
+            --cacert "${cacert}" \
+            --header "Authorization: Bearer ${access_token}" \
+            --request PUT \
+            "${cf_api}/v2/config/${config}/${sec_group_id}" \
+            1> /dev/null
+    done
+}
+
+# unbind_security_group unbinds the security group from the staging and running configs.
+function unbind_security_group() {
+    local cf_api="$1"
+    local cacert="$2"
+    local access_token="$3"
+    local sec_group_id="$4"
+    local configs=(
+        "staging_security_groups"
+        "running_security_groups"
+    )
+    for config in "${configs[@]}"; do
+        curl \
+            --silent --show-error --fail \
+            --cacert "${cacert}" \
+            --header "Authorization: Bearer ${access_token}" \
+            --request DELETE \
+            "${cf_api}/v2/config/${config}/${sec_group_id}" \
+            1> /dev/null
+    done
+}
+
+# Write the CF_API_CA_CERT and UAA_CA_CERT environment variable contents to PEM files.
+if [ "${CF_API_CA_CERT:-}" != "" ]; then
+    cf_api_ca_cert_pem="${DATA_DIR}/cf_api_ca_cert.pem"
+    echo -n "${CF_API_CA_CERT}" > "${cf_api_ca_cert_pem}"
+    uaa_ca_cert_pem="${DATA_DIR}/uaa_ca_cert.pem"
+    echo -n "${UAA_CA_CERT}" > "${uaa_ca_cert_pem}"
+fi
 
 # Wait for the CF API to be accessible.
 echo "Waiting for the API to be accessible..."
@@ -58,168 +199,95 @@ sec_group_json=$(cat <<EOF
 EOF
 )
 
-# get_access_token returns a token to be used on the CF API calls.
-function get_access_token() {
-    scope="scope=cloud_controller.admin"
-    grant_type="grant_type=client_credentials"
-    client_id="client_id=${CF_USERNAME}"
-    client_secret="client_secret=${CF_PASSWORD}"
-    curl \
-        --silent --show-error --fail \
-        --cacert "${uaa_ca_cert_pem}" \
-        --request POST \
-        --data "${scope}&${grant_type}&${client_id}&${client_secret}" \
-        "${UAA_URL}/oauth/token" \
-        | jq -r '.access_token'
-}
-
-# check_security_group checks if the security group already exists.
-function check_security_group() {
-    local access_token
-    access_token="$1"
-    local total_results
-    total_results=$(
-        curl \
-            --silent --show-error --fail \
-            --cacert "${cf_api_ca_cert_pem}" \
-            --header "Authorization: Bearer ${access_token}" \
-            "${CF_API}/v2/security_groups?q=name:${sec_group_name}" \
-            | jq -r '.total_results'
-    )
-    if [[ "${total_results}" == "0" ]]; then return 1; fi
-}
-
-# get_sec_group_id returns the security group ID.
-function get_sec_group_id() {
-    local access_token
-    access_token="$1"
-    curl \
-        --silent --show-error --fail \
-        --cacert "${cf_api_ca_cert_pem}" \
-        --header "Authorization: Bearer ${access_token}" \
-        "${CF_API}/v2/security_groups?q=name:${sec_group_name}" \
-        | jq -r '.resources[0].metadata.guid'
-}
-
-# create_security_group creates the new security group.
-function create_security_group() {
-    local access_token
-    access_token="$1"
-    curl \
-        --silent --show-error --fail \
-        --cacert "${cf_api_ca_cert_pem}" \
-        --header "Authorization: Bearer ${access_token}" \
-        --header "Content-Type: application/json" \
-        --request POST \
-        --data "${sec_group_json}" \
-         "${CF_API}/v2/security_groups" \
-        1> /dev/null
-}
-
-# update_security_group updates the security group with the up-to-date JSON definition.
-function update_security_group() {
-    local access_token
-    access_token="$1"
-    local sec_group_id
-    sec_group_id="$2"
-    curl \
-        --silent --show-error --fail \
-        --cacert "${cf_api_ca_cert_pem}" \
-        --header "Authorization: Bearer ${access_token}" \
-        --header "Content-Type: application/json" \
-        --request PUT \
-        --data "${sec_group_json}" \
-        "${CF_API}/v2/security_groups/${sec_group_id}" \
-        1> /dev/null
-}
-
-# delete_security_group deletes the security group. It assumes the staging and running configs are
-# unbound.
-function delete_security_group() {
-    local access_token
-    access_token="$1"
-    local sec_group_id
-    sec_group_id="$2"
-    curl \
-        --silent --show-error --fail \
-        --cacert "${cf_api_ca_cert_pem}" \
-        --header "Authorization: Bearer ${access_token}" \
-        --request DELETE \
-        "${CF_API}/v2/security_groups/${sec_group_id}" \
-        1> /dev/null
-}
-
-# bind_security_group binds the security group to the staging and running configs.
-function bind_security_group() {
-    local access_token
-    access_token="$1"
-    local sec_group_id
-    sec_group_id="$2"
-    local configs
-    configs=(
-        "staging_security_groups"
-        "running_security_groups"
-    )
-    for config in "${configs[@]}"; do
-        curl \
-            --silent --show-error --fail \
-            --cacert "${cf_api_ca_cert_pem}" \
-            --header "Authorization: Bearer ${access_token}" \
-            --request PUT \
-            "${CF_API}/v2/config/${config}/${sec_group_id}" \
-            1> /dev/null
-    done
-}
-
-# unbind_security_group unbinds the security group from the staging and running configs.
-function unbind_security_group() {
-    local access_token
-    access_token="$1"
-    local sec_group_id
-    sec_group_id="$2"
-    local configs
-    configs=(
-        "staging_security_groups"
-        "running_security_groups"
-    )
-    for config in "${configs[@]}"; do
-        curl \
-            --silent --show-error --fail \
-            --cacert "${cf_api_ca_cert_pem}" \
-            --header "Authorization: Bearer ${access_token}" \
-            --request DELETE \
-            "${CF_API}/v2/config/${config}/${sec_group_id}" \
-            1> /dev/null
-    done
-}
-
 # Create the security group if it doesn't exist, otherwise, update it.
 echo "Getting access token..."
-access_token=$(get_access_token)
-echo "Checking if security group already exists..."
-if ! check_security_group "${access_token}"; then
+access_token=$(
+    get_access_token \
+        "${UAA_URL}/oauth/token" \
+        "${uaa_ca_cert_pem}" \
+        "${CF_USERNAME}" \
+        "${CF_PASSWORD}"
+)
+echo "Checking if security group ${sec_group_name} already exists..."
+if ! check_security_group \
+        "${CF_API}" \
+        "${cf_api_ca_cert_pem}" \
+        "${access_token}" \
+        "${sec_group_name}"; then
     echo "Creating security group ${sec_group_name}..."
-    create_security_group "${access_token}"
+    create_security_group \
+        "${CF_API}" \
+        "${cf_api_ca_cert_pem}" \
+        "${access_token}" \
+        "${sec_group_json}"
     echo "Binding security group ${sec_group_name}..."
-    sec_group_id=$(get_sec_group_id "${access_token}")
-    bind_security_group "${access_token}" "${sec_group_id}"
+    sec_group_id=$(
+        get_sec_group_id \
+            "${CF_API}" \
+            "${cf_api_ca_cert_pem}" \
+            "${access_token}" \
+            "${sec_group_name}"
+    )
+    bind_security_group \
+        "${CF_API}" \
+        "${cf_api_ca_cert_pem}" \
+        "${access_token}" \
+        "${sec_group_id}"
 else
     echo "Updating security group ${sec_group_name}..."
-    sec_group_id=$(get_sec_group_id "${access_token}")
-    update_security_group "${access_token}" "${sec_group_id}"
+    sec_group_id=$(
+        get_sec_group_id \
+            "${CF_API}" \
+            "${cf_api_ca_cert_pem}" \
+            "${access_token}" \
+            "${sec_group_name}"
+    )
+    update_security_group \
+        "${CF_API}" \
+        "${cf_api_ca_cert_pem}" \
+        "${access_token}" \
+        "${sec_group_id}" \
+        "${sec_group_json}"
+    echo "Binding security group ${sec_group_name}..."
+    bind_security_group \
+        "${CF_API}" \
+        "${cf_api_ca_cert_pem}" \
+        "${access_token}" \
+        "${sec_group_id}"
 fi
 
 # Setup a cleanup for the security group on exit.
 cleanup() {
     echo "Getting access token..."
-    access_token=$(get_access_token)
+    access_token=$(
+        get_access_token \
+            "${UAA_URL}/oauth/token" \
+            "${uaa_ca_cert_pem}" \
+            "${CF_USERNAME}" \
+            "${CF_PASSWORD}"
+    )
     echo "Unbinding security group ${sec_group_name}..."
-    sec_group_id=$(get_sec_group_id "${access_token}")
-    unbind_security_group "${access_token}" "${sec_group_id}"
+    sec_group_id=$(
+        get_sec_group_id \
+            "${CF_API}" \
+            "${cf_api_ca_cert_pem}" \
+            "${access_token}" \
+            "${sec_group_name}"
+    )
+    unbind_security_group \
+        "${CF_API}" \
+        "${cf_api_ca_cert_pem}" \
+        "${access_token}" \
+        "${sec_group_id}"
     echo "Deleting security group ${sec_group_name}..."
-    delete_security_group "${access_token}" "${sec_group_id}"
+    delete_security_group \
+        "${CF_API}" \
+        "${cf_api_ca_cert_pem}" \
+        "${access_token}" \
+        "${sec_group_id}"
 }
 trap cleanup EXIT
 
 # Wait until pid 1 is terminated. I.e. wait for the pod to receive SIGINT.
+echo "Done! Waiting for cleanup signal..."
 tail --pid 1 --follow /dev/null
