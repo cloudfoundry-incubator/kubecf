b161cd8c29ca44ce64f3cc18b4791a9f4fac9597
viovanov <viovanov@suse.com>
2020-03-26 03:21:05 +0200

    Explicit disk/volume setup for the diego cell

---
+++ bosh/releases/pre_render_scripts/diego-cell/rep/bpm/patch_bpm.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset
+
+target="/var/vcap/all-releases/jobs-src/diego/rep/templates/bpm.yml.erb"
+sentinel="${target}.patch_sentinel"
+if [[ -f "${sentinel}" ]]; then
+  echo "Patch already applied. Skipping"
+  exit 0
+fi
+
+# Patch BPM, since we're actually running in-cluster without BPM
+patch --verbose "${target}" <<'EOT'
+@@ -6,7 +6,7 @@
+     open_files: 100000
+   hooks:
+     pre_start: /var/vcap/jobs/rep/bin/bpm-pre-start
+-  ephemeral_disk: true
++  ephemeral_disk: false
+   additional_volumes:
+     - path: <%= p("diego.executor.volman.driver_paths") %>
+     - path : /var/vcap/data/garden
+EOT
+
+touch "${sentinel}"
