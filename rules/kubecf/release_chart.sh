533993bdd93397ad96a2968019ed3f5d6c1c9b5e
André Duffeck <aduffeck@suse.com>
2020-04-02 16:06:32 +0200

    Add support for helmcharts including an imaglist of used docker images
    Signed-off-by: Christian Richter <crichter@suse.com>

---
+++ rules/kubecf/release_chart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is a helper script which combines a helm chart tarball and an image list
+# into a new tarball.
+
+if [ ! -e "$1" ]; then
+  echo "Helm chart tarball '$1' does not exist, bailing out!"; exit 1
+fi
+
+if [ ! -e "$2" ]; then
+  echo "Image list '$2' has not been created, bailing out!"; exit 1
+fi
+
+BASENAME=$(dirname $(tar tf "$1" | head -n1))
+KUBECF_IMAGELIST_TXT_PATH="${BASENAME}/imagelist.txt"
+JQ_PATH="$4"
+
+tar xfv "$1"
+
+$JQ_PATH '.images | .[]' -r < $2 > "${KUBECF_IMAGELIST_TXT_PATH}"
+
+tar cfv "$3" "${BASENAME}/"
