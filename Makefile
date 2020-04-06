d9c295b9b5317c3c59f5022a8d3b77303f47f547
Jan Dubois <jan@jandubois.com>
2020-04-06 00:41:47 -0700

    Add cf-login script

--- Makefile
+++ Makefile
@@ -1,20 +1,23 @@
+cf-login:
+	@./scripts/cf-login.sh
+
+cf-operator-apply:
+	@./scripts/cf-operator-apply.sh
+
+kubecf-apply:
+	@./scripts/kubecf-apply.sh
+
+kubecf-build:
+	@./scripts/kubecf-build.sh
+
+kubecf-delete:
+	@./scripts/kubecf-delete.sh
+
 minikube-start:
 	@./scripts/minikube-start.sh
 
 minikube-delete:
 	@./scripts/minikube-delete.sh
-
-cf-operator-apply:
-	@./scripts/cf-operator-apply.sh
-
-kubecf-build:
-	@./scripts/kubecf-build.sh
-
-kubecf-apply:
-	@./scripts/kubecf-apply.sh
-
-kubecf-delete:
-	@./scripts/kubecf-delete.sh
 
 testing-smoke:
 	@./scripts/testing-smoke.sh
---
+++ scripts/cf-login.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+: "${GIT_ROOT:=$(git rev-parse --show-toplevel)}"
+# shellcheck disable=SC1090
+source "${GIT_ROOT}/scripts/include/setup.sh"
+
+require_tools base64 cf kubectl
+
+SYSTEM_DOMAIN=$(kubectl get secret --namespace "${KUBECF_NS}" kubecf.var-system-domain \
+                        --output jsonpath='{.data.value}' | base64 --decode)
+
+ADMIN_PASSWORD=$(kubectl get secret --namespace "${KUBECF_NS}" kubecf.var-cf-admin-password \
+                         --output jsonpath='{.data.password}' | base64 --decode)
+
+cf login --skip-ssl-validation -a "https://api.${SYSTEM_DOMAIN}" -u admin -p "${ADMIN_PASSWORD}"
