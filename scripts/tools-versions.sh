407847d8c9a5b955bcdb8a28c88a85f9d2b305e5
Jan Dubois <jan.dubois@suse.com>
2020-04-06 14:20:20 -0700

    tools-versions throws an error when tools are missing or too old

--- scripts/tools-versions.sh
+++ scripts/tools-versions.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 source scripts/include/setup.sh
 
-TOOLS+=(fubar) # XXX remove; for testing
-
 # Add additional tool dependencies from the internal tool definitions.
 for tool in "${TOOLS[@]}"; do
     # shellcheck disable=SC2207
@@ -13,10 +11,19 @@ done
 # shellcheck disable=SC2207
 TOOLS=($(printf '%s\n' "${TOOLS[@]}" | sort | uniq))
 
+RC=0
 for tool in "${TOOLS[@]}"; do
-    STATUS="$(tool_status "${tool}")" || true
+    if ! STATUS="$(tool_status "${tool}")"; then
+        RC=1
+    fi
+
     # Don't show internal tools unless VERBOSE is set.
     if [[ -n "${VERBOSE:-}" || ! "${STATUS}" =~ is[[:space:]]internal ]]; then
         echo "${STATUS}"
     fi
 done
+
+if [ "${RC}" -eq 1 ]; then
+    echo
+    die "Some tools are missing or too old"
+fi
