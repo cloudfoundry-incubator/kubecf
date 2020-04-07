f1dfe460d13300c880699535cfc31e253fadd94b
Dimitris Karakasilis <DKarakasilis@suse.com>
2020-04-07 17:11:22 +0300

    Make linter happy
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- .concourse/tasks/upgrade.sh
+++ .concourse/tasks/upgrade.sh
@@ -4,8 +4,10 @@ set -e
 
 echo "Running the upgrade test"
 
-export KUBECF_LATEST_RELEASE=$(cat kubecf-github-release/version)
+KUBECF_LATEST_RELEASE=$(cat kubecf-github-release/version)
+export KUBECF_LATEST_RELEASE
 export SCF_CHART="https://github.com/cloudfoundry-incubator/kubecf/releases/download/${KUBECF_LATEST_RELEASE}/kubecf-bundle-${KUBECF_LATEST_RELEASE}.tgz"
+
 export ENABLE_EIRINI=false
 export SCF_OPERATOR=true
 
@@ -13,8 +15,10 @@ export FORCE_DELETE=true
 export HELM_VERSION="v3.1.1"
 export BACKEND=imported
 export QUIET_OUTPUT=true
-export CLUSTER_NAME="$(cat kind-environments/name)"
-export KUBECFG="$(readlink -f kind-environments/metadata)"
+CLUSTER_NAME="$(cat kind-environments/name)"
+export CLUSTER_NAME
+KUBECFG="$(readlink -f kind-environments/metadata)"
+export KUBECFG
 
 pushd catapult
 # Bring up a k8s cluster and builds+deploy kubecf
@@ -23,5 +27,6 @@ make kubeconfig scf
 
 # Now upgrade to whatever chart we built for commit-to-test
 # The chart should be in s3.kubecf-ci directory
-export SCF_CHART="$(readlink -f ../s3.kubecf-ci/*.tgz)"
+SCF_CHART="$(readlink -f ../s3.kubecf-ci/*.tgz)"
+export SCF_CHART
 make scf-upgrade
