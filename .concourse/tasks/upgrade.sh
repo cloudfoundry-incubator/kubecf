4c0cadc7413b63e2e0d6993e488c673a00b84afe
Ettore Di Giacinto <edigiacinto@suse.com>
2020-04-07 16:57:19 +0300

    Add code to the upgrade-test and unclaim the clusterIwhen done
    Signed-off-by: Dimitris Karakasilis <DKarakasilis@suse.com>

--- .concourse/tasks/upgrade.sh
+++ .concourse/tasks/upgrade.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
+set -e
+
 echo "Running the upgrade test"
-exit 0
+
+export KUBECF_LATEST_RELEASE=$(cat kubecf-github-release/version)
+export SCF_CHART="https://github.com/cloudfoundry-incubator/kubecf/releases/download/${KUBECF_LATEST_RELEASE}/kubecf-bundle-${KUBECF_LATEST_RELEASE}.tgz"
+export ENABLE_EIRINI=false
+export SCF_OPERATOR=true
+
+export FORCE_DELETE=true
+export HELM_VERSION="v3.1.1"
+export BACKEND=imported
+export QUIET_OUTPUT=true
+export CLUSTER_NAME="$(cat kind-environments/name)"
+export KUBECFG="$(readlink -f kind-environments/metadata)"
+
+pushd catapult
+# Bring up a k8s cluster and builds+deploy kubecf
+# https://github.com/SUSE/catapult/wiki/Build-and-run-SCF#build-and-run-kubecf
+make kubeconfig scf
+
+# Now upgrade to whatever chart we built for commit-to-test
+# The chart should be in s3.kubecf-ci directory
+export SCF_CHART="$(readlink -f ../s3.kubecf-ci/*.tgz)"
+make scf-upgrade
