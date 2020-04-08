4584ad6211d5592f4e56271a6773bbfac423504d
Jan Dubois <jan.dubois@suse.com>
2020-04-07 17:07:08 -0700

    typo fix

--- scripts/kubecf-delete.sh
+++ scripts/kubecf-delete.sh
@@ -3,6 +3,6 @@ source scripts/include/setup.sh
 
 require_tools helm kubectl xargs_no_run_if_empty
 
-helm delete kubecf --namespace "${KUBECF}"
+helm delete kubecf --namespace "${KUBECF_NS}"
 kubectl get --namespace "${KUBECF_NS}" pvc --output name | \
     xargs_no_run_if_empty kubectl delete --namespace "${KUBECF_NS}"
