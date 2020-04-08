c0df4eba49363b8c193dfefc196b61b049246cbd
Jan Dubois <jan.dubois@suse.com>
2020-04-07 21:52:44 -0700

    Inline minikube/start.sh

--- scripts/minikube-start.sh
+++ scripts/minikube-start.sh
@@ -3,14 +3,26 @@ source scripts/include/setup.sh
 
 require_tools minikube
 
-# shellcheck disable=SC2034
-MINIKUBE="minikube"
 : "${K8S_VERSION:=1.15.6}"
 : "${VM_CPUS:=4}"
 : "${VM_MEMORY:=16384}"
 : "${VM_DISK_SIZE:=120g}"
-# shellcheck disable=SC2034
-ISO_URL=${MINIKUBE_ISO_URL}
 
-# shellcheck disable=SC1091
-source ./dev/minikube/start.sh
+if ! minikube status > /dev/null; then
+    # shellcheck disable=SC2086
+    minikube start \
+             --kubernetes-version "${K8S_VERSION}" \
+             --cpus "${VM_CPUS}" \
+             --memory "${VM_MEMORY}" \
+             --disk-size "${VM_DISK_SIZE}" \
+             --iso-url "${MINIKUBE_ISO_URL}" \
+             ${VM_DRIVER:+--vm-driver "${VM_DRIVER}"} \
+             --extra-config=apiserver.runtime-config=settings.k8s.io/v1alpha1=true \
+             --extra-config=apiserver.enable-admission-plugins=MutatingAdmissionWebhook,PodPreset \
+             ${MINIKUBE_EXTRA_OPTIONS:-}
+
+    # Enable hairpin by setting the docker0 promiscuous mode on.
+    minikube ssh -- "sudo ip link set docker0 promisc on"
+else
+    echo "Minikube is already started"
+fi
