5f60e0f7e600c85989e45c29ce18399fa8a38e00
Jan Dubois <jan.dubois@suse.com>
2020-04-07 17:07:47 -0700

    DRY when setting default values

--- scripts/include/defaults.sh
+++ scripts/include/defaults.sh
@@ -1,4 +1,4 @@
 # shellcheck shell=bash
 
-CF_OPERATOR_NS=${CF_OPERATOR_NS:-cf-operator}
-KUBECF_NS=${KUBECF_NS:-kubecf}
+: "${CF_OPERATOR_NS:=cf-operator}"
+: "${KUBECF_NS:=kubecf}"
--- scripts/minikube-start.sh
+++ scripts/minikube-start.sh
@@ -5,10 +5,10 @@ require_tools minikube
 
 # shellcheck disable=SC2034
 MINIKUBE="minikube"
-K8S_VERSION=${K8S_VERSION:-1.15.6}
-VM_CPUS=${VM_CPUS:-4}
-VM_MEMORY=${VM_MEMORY:-16384}
-VM_DISK_SIZE=${VM_DISK_SIZE:-120g}
+: "${K8S_VERSION:=1.15.6}"
+: "${VM_CPUS:=4}"
+: "${VM_MEMORY:=16384}"
+: "${VM_DISK_SIZE:=120g}"
 # shellcheck disable=SC2034
 ISO_URL=${MINIKUBE_ISO_URL}
 
