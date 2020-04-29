39cc871810a62883938a1b01feb01141dad4a0ab
vaniisgh <vanisingh@live.co.uk>
2020-04-29 06:54:14 +0530

    add fluentd
    add fluentd daemonset config

--- logging/runlogger.sh
+++ logging/runlogger.sh
@@ -2,3 +2,8 @@ kubectl create -f logging.yaml
 kubectl create -f elasticsearch_svc.yaml
 kubectl create -f elasticsearch_statefulset.yaml
 kubectl create -f kibana.yaml
+kubectl create -f fluentd.yaml
+
+#apply port forwarding to make kibana available on localhoast 5601
+#apply some grep command to find the name of the kibana cluster
+#kubectl port-forward  5601:5601 --namespace=kube-logging
