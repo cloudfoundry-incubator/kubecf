a0559ce1eb36474ac939fd15fcbcf30d3278aa30
Thulio Ferraz Assis <thulio.assis@suse.com>
2020-05-06 03:40:07 -0500

    fix: charset migration script

--- deploy/helm/kubecf/assets/scripts/jobs/pxc/migrate_charset.sh
+++ deploy/helm/kubecf/assets/scripts/jobs/pxc/migrate_charset.sh
@@ -5,41 +5,100 @@
 
 set -o errexit -o nounset -o pipefail
 
+cat > "${HOME}/.my.cnf" <<EOF
+[mysql]
+host=${DATABASE_HOST}
+user=root
+password=${DATABASE_ROOT_PASSWORD}
+EOF
+
 echo "Waiting for database to be ready..."
-until echo "SELECT 'Ready!'" | mysql --host="${DATABASE_HOST}" --user=root --password="${DATABASE_ROOT_PASSWORD}" --connect-timeout=3 2> /dev/null; do
+until echo "SELECT 'Ready!'" | mysql --connect-timeout=3 1> /dev/null 2> /dev/null; do
   sleep 1
 done
+
+function show_tables() {
+  local database=$1
+  mysql \
+    --database="${database}" \
+    --batch \
+    --skip-column-names \
+    --execute "SHOW TABLES"
+}
+
+function show_columns() {
+  local database=$1
+  local table=$2
+  mysql \
+    --database="${database}" \
+    --batch \
+    --skip-column-names \
+    --execute "SHOW COLUMNS FROM ${table}"
+}
+
+function alter_tables() {
+  local database=$1
+
+  while read -r table; do
+    >&2 echo "Generating statements for \`${database}\`.\`${table}\`..."
+
+    echo "ALTER TABLE \`${table}\` DEFAULT CHARACTER SET ${CHARACTER_SET};"
+
+    while read -r column; do
+      field_type=$(awk --field-separator="\t" '{ print toupper($2) }' <<<"${column}")
+
+      awk_program='/^(CHAR|VARCHAR|TINYTEXT|TEXT|MEDIUMTEXT|LONGTEXT|ENUM|SET)/ { print "modify" }'
+      if [[ "$(awk "${awk_program}" <<<"${field_type}")" == "modify" ]]; then
+        field_name=$(awk --field-separator="\t" '{ print $1 }' <<<"${column}")
+        null_opt=$(awk --field-separator="\t" '{ print ($3 == "YES") ? "NULL" : "NOT NULL"}' <<<"${column}")
+        field_default=$(awk --field-separator="\t" '{ print $5 }' <<<"${column}")
+        if [[ "${field_default}" == "NULL" ]]; then
+          if [[ "${null_opt}" == "NOT NULL" ]]; then
+            default_opt=""
+          else
+            default_opt="DEFAULT NULL"
+          fi
+        else
+          default_opt="DEFAULT '${field_default}'"
+        fi
+
+        echo "ALTER TABLE \`${table}\` MODIFY \`${field_name}\` ${field_type} CHARACTER SET ${CHARACTER_SET} ${null_opt} ${default_opt};"
+      fi
+    done < <(show_columns "${database}" "${table}")
+  done < <(show_tables "${database}")
+}
+
+function get_charset() {
+  local database=$1
+  mysql \
+    --database="${database}" \
+    --batch \
+    --skip-column-names \
+    --execute "SELECT default_character_set_name FROM information_schema.SCHEMATA WHERE schema_name = '${database}';"
+}
 
 function alter_database() {
   local database=$1
 
-  cat <<EOT
-ALTER DATABASE \`${database}\`
-  DEFAULT CHARACTER SET ${CHARACTER_SET}
-  DEFAULT COLLATE ${COLLATE};
+  current_charset=$(get_charset "${database}")
+  if [[ "${current_charset}" == "${CHARACTER_SET}" ]]; then
+    return 0
+  fi
 
-EOT
-
+  echo "ALTER DATABASE \`${database}\` DEFAULT CHARACTER SET ${CHARACTER_SET} DEFAULT COLLATE ${COLLATE};"
   echo "USE \`${database}\`;"
   echo "SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';"
+  echo "SET foreign_key_checks = 0;"
 
-  mysql \
-    --host="${DATABASE_HOST}" \
-    --user=root \
-    --password="${DATABASE_ROOT_PASSWORD}" \
-    --database="${database}" \
-    --batch \
-    --skip-column-names \
-    --execute "SHOW TABLES" \
-    | xargs --max-lines=1 --replace echo "SET foreign_key_checks = 0; ALTER TABLE \`{}\` CONVERT TO CHARACTER SET ${CHARACTER_SET} COLLATE ${COLLATE}; SET foreign_key_checks = 1;"
+  alter_tables "${database}"
 
+  echo "SET foreign_key_checks = 1;"
   echo -e "\n"
 }
 
 STATEMENT=$(
 echo "START TRANSACTION;"
 
-alter_database "mysql"
 alter_database "kubecf"
 
 for database in ${DATABASES}; do
@@ -50,4 +109,4 @@ done
 echo "COMMIT;"
 )
 
-echo "${STATEMENT}" | mysql --host="${DATABASE_HOST}" --user=root --password="${DATABASE_ROOT_PASSWORD}"
+mysql <<<"${STATEMENT}"
