4492b4091b52579d9adbc3975773cf2a592080df
Thulio Ferraz Assis <thulio.assis@suse.com>
2020-05-06 03:39:16 -0500

    fix: improve seeder

--- deploy/helm/kubecf/assets/scripts/jobs/pxc/seeder.sh
+++ deploy/helm/kubecf/assets/scripts/jobs/pxc/seeder.sh
@@ -2,43 +2,47 @@
 
 set -o errexit -o nounset -o pipefail
 
+cat > "${HOME}/.my.cnf" <<EOF
+[mysql]
+host=${DATABASE_HOST}
+user=root
+password=${DATABASE_ROOT_PASSWORD}
+EOF
+
 echo "Waiting for database to be ready..."
-until echo "SELECT 'Ready!'" | mysql --host="${DATABASE_HOST}" --user=root --password="${DATABASE_ROOT_PASSWORD}" --connect-timeout=3 2> /dev/null; do
+until echo "SELECT 'Ready!'" | mysql --connect-timeout=3 1> /dev/null 2> /dev/null; do
   sleep 1
 done
 
-mysql --host="${DATABASE_HOST}" --user=root --password="${DATABASE_ROOT_PASSWORD}" \
-  < <(
-    echo "START TRANSACTION;"
+mysql < <(
+  echo "START TRANSACTION;"
+  echo "\
+    CREATE DATABASE IF NOT EXISTS kubecf
+      DEFAULT CHARACTER SET ${CHARACTER_SET}
+      DEFAULT COLLATE ${COLLATE};
+    USE kubecf;
+    CREATE TABLE IF NOT EXISTS db_leader_election (
+      anchor tinyint(3) unsigned NOT NULL,
+      host varchar(128) NOT NULL,
+      last_seen_active timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (anchor)
+    );"
+  for database in ${DATABASES}; do
+    if [[ -z "${database}" ]]; then continue; fi
+
+    password=$(</passwords/"${database}"/password)
+
     echo "\
-      CREATE DATABASE IF NOT EXISTS kubecf;
-      ALTER DATABASE kubecf
+      CREATE USER IF NOT EXISTS \`${database}\`;
+      ALTER USER \`${database}\` IDENTIFIED BY '${password}';
+
+      CREATE DATABASE IF NOT EXISTS \`${database}\`
         DEFAULT CHARACTER SET ${CHARACTER_SET}
         DEFAULT COLLATE ${COLLATE};
-      USE kubecf;
-      CREATE TABLE IF NOT EXISTS db_leader_election (
-        anchor tinyint(3) unsigned NOT NULL,
-        host varchar(128) NOT NULL,
-        last_seen_active timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        PRIMARY KEY (anchor)
-      );"
-    for database in ${DATABASES}; do
-      if [[ -z "${database}" ]]; then continue; fi
 
-      password=$(</passwords/"${database}"/password)
-
-      echo "\
-        CREATE USER IF NOT EXISTS \`${database}\`;
-        ALTER USER \`${database}\` IDENTIFIED BY '${password}';
-
-        CREATE DATABASE IF NOT EXISTS \`${database}\`;
-        ALTER DATABASE \`${database}\`
-          DEFAULT CHARACTER SET ${CHARACTER_SET}
-          DEFAULT COLLATE ${COLLATE};
-
-        GRANT ALL ON \`${database}\`.* TO '${database}'@'%';
-      "
-    done
-    echo "COMMIT;"
-  )
+      GRANT ALL ON \`${database}\`.* TO '${database}'@'%';
+    "
+  done
+  echo "COMMIT;"
+)
 echo "Done!"
