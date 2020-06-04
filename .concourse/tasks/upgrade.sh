d61a83b3e8e2abe14e5b94f2de54f0c79903bbd0
Dimitris Karakasilis <jimmykarily@gmail.com>
2020-06-04 13:48:27 +0300

    Re-apply "Revert "Setup proper dns on gke""
    This reverts commit 9f1e8e91e31831fc09dde82415af88d5fb8dcaba.
    
    The PR was accidentally merged before it was ready.

--- .concourse/tasks/upgrade.sh
+++ .concourse/tasks/upgrade.sh
@@ -27,6 +27,8 @@ gcloud auth activate-service-account --key-file "${PWD}/gke-key.json"
 export GKE_PROJECT='{{ .gke_project | default "suse-225215" }}'
 export GKE_ZONE='{{ .gke_zone | default "europe-west3-c"}}'
 
+export DOMAIN="${GKE_CLUSTER_NAME}.kubecf.ci"
+
 gcloud --quiet beta container \
   --project "${GKE_PROJECT}" clusters create "${GKE_CLUSTER_NAME}" \
   --zone "${GKE_ZONE}" \
@@ -63,9 +65,19 @@ EOF
 export CONFIG_OVERRIDE
 
 pushd catapult
+export CLUSTER_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 # Bring up a k8s cluster and builds+deploy kubecf
 # https://github.com/SUSE/catapult/wiki/Build-and-run-SCF#build-and-run-kubecf
 make kubeconfig kubecf
+
+# Setup dns
+tcp_router_ip=$(kubectl  get svc -n scf tcp-router-public -o json | jq -r .status.loadBalancer.ingress[].ip | head -n 1)
+public_router_ip=$(kubectl  get svc -n scf router-public -o json | jq -r .status.loadBalancer.ingress[].ip | head -n 1)
+
+gcloud --quiet beta dns --project=suse-225215 record-sets transaction start --zone=kubecf-ci
+gcloud --quiet beta dns --project=suse-225215 record-sets transaction add --name="*.${DOMAIN}." --ttl=300 --type=A --zone=kubecf-ci "$public_router_ip"
+gcloud --quiet beta dns --project=suse-225215 record-sets transaction add --name="tcp.${DOMAIN}." --ttl=300 --type=A --zone=kubecf-ci "$tcp_router_ip"
+gcloud --quiet beta dns --project=suse-225215 record-sets transaction execute --zone=kubecf-ci
 
 # Now upgrade to whatever chart we built for commit-to-test
 # The chart should be in s3.kubecf-ci directory
