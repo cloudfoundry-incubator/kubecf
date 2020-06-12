989fed013c0b5f201f53f4a42efcc37b2c5a8e36
Jan Dubois <jan.dubois@suse.com>
2020-06-12 15:52:45 -0700

    Put find arguments into a separate array to make code more readable

--- dev/linters/shellcheck.sh
+++ dev/linters/shellcheck.sh
@@ -4,9 +4,11 @@ set -o errexit -o nounset
 
 workspace=$(bazel info workspace)
 
+find_args=(
+    -not \( -path "${workspace}/deploy/helm/kubecf/charts" -prune \)
+    -name '*.sh'
+)
+
 # shellcheck disable=SC2046
 # We want word splitting with find.
-bazel run @shellcheck//:binary -- \
-      $(find "${workspace}" \
-             -not \( -path "${workspace}/deploy/helm/kubecf/charts" -prune \) \
-             -name '*.sh')
+bazel run @shellcheck//:binary -- $(find "${workspace}" "${find_args[@]}")
--- dev/linters/yamllint.sh
+++ dev/linters/yamllint.sh
@@ -4,12 +4,15 @@ set -o errexit -o nounset
 
 workspace=$(bazel info workspace)
 
+find_args=(
+    -not \( -path "${workspace}/deploy/helm/kubecf/charts" -prune \)
+    -path "${workspace}/deploy/helm/kubecf/values.*"
+    -or -not -path "${workspace}/deploy/helm/kubecf/*"
+    -name '*.yaml' -or -name '*.yml'
+)
+
 # shellcheck disable=SC2046
 # We want word splitting with find.
 bazel run //dev/linters/yamllint -- \
       -d "{extends: relaxed, rules: {line-length: {max: 120}}}" \
-      --strict $(find "${workspace}" \
-                      -not \( -path "${workspace}/deploy/helm/kubecf/charts" -prune \) \
-                      -path "${workspace}/deploy/helm/kubecf/values.*" \
-                      -or -not -path "${workspace}/deploy/helm/kubecf/*" \
-                      -name '*.yaml' -or -name '*.yml')
+      --strict $(find "${workspace}" "${find_args[@]}")
