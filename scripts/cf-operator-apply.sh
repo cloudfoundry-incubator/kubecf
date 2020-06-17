745618bcc9578026924c10f3e910f36e974072da
Jan Dubois <jan.dubois@suse.com>
2020-06-17 15:29:58 -0700

    Create cf-operator namespace only when it doesn't yet exist
    Also operator url may now contain a {version} template that must be
    substituted to get the real url.

--- scripts/cf-operator-apply.sh
+++ scripts/cf-operator-apply.sh
@@ -3,8 +3,11 @@ source scripts/include/setup.sh
 
 require_tools kubectl helm
 
-kubectl create ns "${CF_OPERATOR_NS}"
+if ! kubectl get ns "${CF_OPERATOR_NS}" &> /dev/null; then
+    kubectl create ns "${CF_OPERATOR_NS}" > /dev/null
+fi
 
-helm install cf-operator "${CF_OPERATOR_URL}" \
+helm install cf-operator \
+     "${CF_OPERATOR_URL//\{version\}/${CF_OPERATOR_VERSION}}" \
      --namespace "${CF_OPERATOR_NS}" \
      --set "global.operator.watchNamespace=${KUBECF_NS}"
