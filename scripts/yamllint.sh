dc1a3ef86183364c9adc42e709b5c632d305498c
Jan Dubois <jan.dubois@suse.com>
2020-06-23 16:45:31 -0700

    Add comments to complex find expression

--- scripts/yamllint.sh
+++ scripts/yamllint.sh
@@ -4,13 +4,24 @@ source scripts/include/setup.sh
 require_tools yamllint
 
 find_args=(
+    # Don't lint the helm subcharts; they are imported.
     -not \( -path "./deploy/helm/kubecf/charts" -prune \)
+
+    # Don't lint any generated output files.
     -not \( -path "./output" -prune \)
+
+    # Don't lint submodules.
     -not \( -path "./src" -prune \)
+
+    # Only lint values.yaml file in the kubecf static files
+    # the rest contain template expressions that must be
+    # evaluated before the files become valid YAML.
     \( -path "./deploy/helm/kubecf/values.*"
        -or
        -not -path "./deploy/helm/kubecf/*"
     \)
+
+    # Check both file extensions, although we should have only .yaml files.
     \( -name '*.yaml' -or -name '*.yml' \)
 )
 
