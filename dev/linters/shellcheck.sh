419000aa2ecb085843a7b54185f640b346a166f7
Jan Dubois <jan.dubois@suse.com>
2020-06-24 12:00:07 -0700

    Work around in Bazel issues in dev/linters/shellcheck.sh
    The problem is that the linter only uses Bazel half-heartedly.
    It uses Bazel to invoke the binary, but doesn't specify the
    files to be checked, so they aren't copied into the sandbox.

--- dev/linters/shellcheck.sh
+++ dev/linters/shellcheck.sh
@@ -6,10 +6,29 @@ workspace=$(bazel info workspace)
 
 find_args=(
     -not \( -path "${workspace}/deploy/helm/kubecf/charts" -prune \)
+    -not \( -path "${workspace}/output" -prune \)
     -not \( -path "${workspace}/src" -prune \)
     -name '*.sh'
 )
 
 # shellcheck disable=SC2046
 # We want word splitting with find.
-bazel run @shellcheck//:binary -- $(find "${workspace}" "${find_args[@]}")
+#bazel run @shellcheck//:binary -- $(find "${workspace}" "${find_args[@]}")
+
+# Bazel will run `shellcheck` inside the sandbox, but the files it checks are
+# not copied inside. It passes all files to be checked by absolute filename
+# via $workspace. That way relative filenames don't resolve and produce bogus
+# errors. Workaround follows:
+
+# Determine shellcheck version because it is part of the path inside the cache.
+VERSION=$( < dependencies.yaml bazel run --experimental_ui_limit_console_output=1 @yq//:binary \
+             -- read - binaries.shellcheck.version)
+
+# Make sure shellcheck is downloaded into the Bazel cache.
+bazel run @shellcheck//:binary -- --version &> /dev/null
+
+# Call cached bazel binary of shellcheck from current directory manually.
+# shellcheck disable=SC2046
+# We want word splitting with find.
+"./bazel-kubecf/external/shellcheck/shellcheck-v${VERSION}/shellcheck" \
+    -- $(find "${workspace}" "${find_args[@]}")
