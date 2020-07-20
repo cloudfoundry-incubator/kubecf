c6690e2d3e88cf9ae1339320193bbf99f13f695c
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-07-20 16:00:02 +0200

    Add logic to pause jobs if pushed pipeline is new

--- .concourse/create-pipeline.sh
+++ .concourse/create-pipeline.sh
@@ -48,16 +48,38 @@ else
     fi
 fi
 
+# Determine if the pipeline being pushed is a new pipeline
+existing_pipeline_job_count=$(
+  fly --target ${target} get-pipeline --pipeline ${PIPELINE} --json | jq '.jobs | length' || true
+)
+if [[ ${existing_pipeline_job_count} -gt 0 ]]; then
+  pipeline_already_existed=true
+else
+  pipeline_already_existed=false
+fi
+
+# Push a new pipeline, or update an existing one
 fly_args=(
     "--target=${target}"
     "set-pipeline"
     "--pipeline=${PIPELINE}"
 )
-
 if [[ "$2" =~ "reconciler" ]]; then
     fly "${fly_args[@]}" --config \
         <(gomplate -V --datasource config="$PIPELINE".yaml --file kubecf-pool-reconciler.yaml.gomplate)
 else # kubecf pipeline
     fly "${fly_args[@]}" --config \
         <(gomplate -V --datasource config="$PIPELINE".yaml --file pipeline.yaml.gomplate)
+fi
+
+# If the pipeline being pushed was a *new* pipeline, pause all the 'initial' jobs (jobs without 'passed:' dependencies)
+# Important caveat: If a pipeline is updated to add new targets, the new initial jobs will *not* be paused
+if ! ${pipeline_already_existed}; then
+    jobs_without_dependencies_names=$(
+        fly ${target:+"--target=${target}"} get-pipeline --json -p "${PIPELINE}" | jq -r '.jobs - [.jobs[] | select(.plan[] | .passed)] | .[].name'
+                                   )
+    for job_name in ${jobs_without_dependencies_names}; do
+        fly ${target:+"--target=${target}"} pause-job -j "${PIPELINE}/${job_name}"
+    done
+    fly ${target:+"--target=${target}"} unpause-pipeline --pipeline="${PIPELINE}"
 fi
