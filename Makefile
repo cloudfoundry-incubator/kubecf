cloudfoundry-incubator/kubecf

commit cac7e603fdcc330177fa48a36304a9dafc942970

Rewrite 'all' target to invoke scripts sequentially
The `all: target1 target2 target3` format doesn't guarantee that
things run sequentially when invoked via `make -j ...`.

Makefile
@@ -51,7 +51,12 @@ minikube-delete:
 ########################################################################
 # Run
 
-all: cf-operator-apply cf-operator-wait kubecf-apply kubecf-wait cf-login
+all:
+	./scripts/cf-operator-apply
+	./scripts/cf-operator-wait
+	./scripts/kubecf-apply
+	./scripts/kubecf-wait
+	./scripts/cf-login
 
 cf-login:
 	@./scripts/cf-login.sh

commit 3137cb708d34063e87543d95914c21d76c227428

Fix 'all' Makefile target
It got broken in cac7e60; all scripts are missing the .sh file extension.

Makefile
@@ -52,11 +52,11 @@ minikube-delete:
 # Run
 
 all:
-	./scripts/cf-operator-apply
-	./scripts/cf-operator-wait
-	./scripts/kubecf-apply
-	./scripts/kubecf-wait
-	./scripts/cf-login
+	./scripts/cf-operator-apply.sh
+	./scripts/cf-operator-wait.sh
+	./scripts/kubecf-apply.sh
+	./scripts/kubecf-wait.sh
+	./scripts/cf-login.sh
 
 cf-login:
 	@./scripts/cf-login.sh