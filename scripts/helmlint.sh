1f2ad9883b9486a39346902db7c0c01b27c9803d
Jan Dubois <jan.dubois@suse.com>
2020-08-17 14:13:08 -0700

    Require helm 3.3.0 to get helm linting bug fix
    `helm lint` will now take additional settings provided via --values
    or --set options into account before linting the combined values.
    
    I found still a residual issue with overwriting values that are set
    to nil inside values.yaml, so I replaced `system_domain` with a comment
    line.
    
    See https://github.com/helm/helm/issues/7756#issuecomment-675109870
    
    `helm lint` is also more picky about empty YAML documents, so comments
    outside conditionals must be removed, or moved inside the condition to
    prevent linting errors like this:
    
    [ERROR] templates/eirini.yaml: object name does not conform to Kubernetes naming requirements: ""
    [ERROR] templates/sync-integration-tests.yaml: object name does not conform to Kubernetes naming requirements: ""

--- scripts/helmlint.sh
+++ scripts/helmlint.sh
@@ -12,15 +12,11 @@ TARGET_DIR="${TEMP_DIR}/kubecf"
 mkdir "${TARGET_DIR}"
 
 tar xfz "${TARGET_FILE}" -C "${TEMP_DIR}"
-# Workaround: [Helm lint (v3) doesn't take values files into account during schema validation]
-# (https://github.com/helm/helm/issues/7756). Fixed in helm 3.3.0.
-perl -p -i -e 's/(system_domain:).*/$1 example.com/' "${TARGET_DIR}/values.yaml"
 helm lint "${TARGET_DIR}" --set system_domain=example.com
 
 # Running json schema validator a second time to also check the embedded config files.
 # Done in a separate run because the config files are merged here on top of values.yaml,
-# which will cause replacement of any arrays. Using `helm template` instead, which also
-# runs the validator, but doesn't suffer from bug #7756.
+# which will cause replacement of any arrays.
 
 # shellcheck disable=SC2046
 # We want word splitting with find.
