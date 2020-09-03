c87102ce66bee2c008bb723b67da0c9887a8caae
viovanov <viovanov@suse.com>
2020-09-03 14:51:46 +0300

    Fixes after CR

--- scripts/cf-operator-delete.sh
+++ scripts/cf-operator-delete.sh
@@ -3,15 +3,16 @@ source scripts/include/setup.sh
 
 require_tools kubectl helm
 
-set +e
+if helm_ls 2>/dev/null | grep -qi cf-operator ; then
+    helm_delete cf-operator --namespace cf-operator
+fi
 
-helm delete cf-operator
-kubectl delete ns "${CF_OPERATOR_NS}"
-kubectl delete clusterrole cf-operator-quarks-job-cluster
-kubectl delete clusterrole cf-operator-cluster
-kubectl delete clusterrolebinding cf-operator-quarks-job-cluster
-kubectl delete clusterrolebinding cf-operator-cluster
-kubectl delete crd boshdeployments.quarks.cloudfoundry.org
-kubectl delete crd quarksjobs.quarks.cloudfoundry.org
-kubectl delete crd quarkssecrets.quarks.cloudfoundry.org
-kubectl delete crd quarksstatefulsets.quarks.cloudfoundry.org
+kubectl delete --ignore-not-found ns "${CF_OPERATOR_NS}"
+kubectl delete --ignore-not-found clusterrole cf-operator-quarks-job-cluster
+kubectl delete --ignore-not-found clusterrole cf-operator-cluster
+kubectl delete --ignore-not-found clusterrolebinding cf-operator-quarks-job-cluster
+kubectl delete --ignore-not-found clusterrolebinding cf-operator-cluster
+kubectl delete --ignore-not-foundcrd boshdeployments.quarks.cloudfoundry.org
+kubectl delete --ignore-not-found crd quarksjobs.quarks.cloudfoundry.org
+kubectl delete --ignore-not-found crd quarkssecrets.quarks.cloudfoundry.org
+kubectl delete --ignore-not-found crd quarksstatefulsets.quarks.cloudfoundry.org
