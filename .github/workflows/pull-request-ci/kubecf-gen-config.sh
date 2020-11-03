f07fe28055d1f9c96fddcbef4baba056b8e09cc3
Mark Yen <mark.yen@suse.com>
2020-11-03 14:17:44 -0800

    fix: CI: Simplify use of gomplate in KubeCF config
    If we run multiple merges with gomplate, rather than one large template,
    we can make the changes a lot more readable.
    
    Also, we can drop disabling the minibroker brain tests (they no longer
    exist), as well as dropping the extra .kube config options (no longer
    relevant).  The extra DNS annotations too (they are done in catapult
    now).
    
    We do need to additionally configure a different set of CATS tests for
    Eirini-based deployments; that's carried over from the catapult target
    to run CATS.
    
    Finally, move the generated KubeCF config file outside of the source
    directories, since KubeCF (with the new-ish build system) will not
    read all of the values files in a given directory anyways.

--- .github/workflows/pull-request-ci/kubecf-gen-config.sh
+++ .github/workflows/pull-request-ci/kubecf-gen-config.sh
@@ -6,42 +6,64 @@ set -o errexit -o nounset -o pipefail
 
 make -C "${GITHUB_WORKSPACE}/catapult" kubecf-gen-config
 
-cd "${GITHUB_WORKSPACE}/catapult/build${CLUSTER_NAME:-${BACKEND}}"
+cd "${GITHUB_WORKSPACE}"
+cp "${GITHUB_WORKSPACE}/catapult/build${CLUSTER_NAME:-${BACKEND}}/scf-config-values.yaml" \
+   kubecf-values.yaml
 
-gomplate --context .=scf-config-values.yaml <<"EOF" \
-> "${GITHUB_WORKSPACE}/kubecf/dev/kubecf/kubecf-values.yaml"
-{{- /* Disable brain minibroker tests */}}
-{{- define "minibroker-tests" }}
-properties:
-    brain-tests:
-        acceptance-tests-brain:
-            tests:
-                minibroker:
-                    {{- range slice "mariadb" "mongodb" "postgres" "redis" }}
-                    {{ . }}:
-                        enabled: false
-                    {{- end }}
-{{- end }}
-{{- $ = tmpl.Exec "minibroker-tests" | yaml | merge $ }}
+# Usage: merge < (YAML data)
+function merge() {
+    gomplate --datasource 'merged=merge:additional|original' \
+        --datasource original=kubecf-values.yaml \
+        --datasource additional=stdin:///additional.yaml \
+        --in '{{ datasource "merged" | toYAML }}' \
+        --out kubecf-values-merged.yaml
+    mv kubecf-values-merged.yaml kubecf-values.yaml
+}
 
-{{- /* Disable registry overrides */}}
-{{- $ = coll.Omit "kube" $ }}
+# Run ginkgo in parallel for faster tests
+merge <<< '
+        properties:
+            acceptance-tests:
+                acceptance-tests:
+                    acceptance_tests:
+                        ginkgo:
+                            nodes: 3
+    '
 
-{{- /* Set DNS annotations */}}
-{{- define "svc_annotation" }}
-services:
-    {{ index . 0 }}:
-        annotations:
-            "external-dns.alpha.kubernetes.io/hostname": {{ index . 1 }}
-    {{- end }}
-{{- $d := .system_domain }}
-{{- $ = printf "%s, *.%s" $d $d | slice "router" | tmpl.Exec "svc_annotation" | yaml | merge $ }}
-{{- $ = printf "ssh.%s" $d | slice "ssh-proxy" | tmpl.Exec "svc_annotation" | yaml | merge $ }}
-{{- $ = printf "tcp.%s, *.tcp.%s" $d $d | slice "tcp-router" | tmpl.Exec "svc_annotation" | yaml | merge $ }}
+# Set Eirini overrides
+if [ "${ENABLE_EIRINI}" == "true" ]; then
+    # This is what upstream Eirini was running and was green:
+    # https://github.com/cloudfoundry-incubator/eirini-ci/blob/09dcce6d9e900f693dfc1a6da70b5a526cf7de18/pipelines/dhall-modules/jobs/run-core-cats.dhall#L52-L86
 
-{{- $ | toYAML }}
-EOF
+    # Normally, it only makes sense to disable test groups that are enabled by
+    # default and enable those that aren't:
+    # https://github.com/cloudfoundry/cf-acceptance-tests#test-configuration
+    # Below we keep the full (explicit) list though, to make it easier to switch
+    # groups on and off.
+    suites=(
+        apps
+        capi_no_bridge
+        container_networking
+        detect
+        docker
+        internet_dependent
+        routing
+        sso
+        v3
+        zipkin
+        ssh
+    )
+    merge <<<"
+        properties:
+            acceptance-tests:
+                acceptance-tests:
+                    acceptance_tests:
+                        include: '$(IFS=, ; echo "=${suites[*]}")'
+                        stacks: [ sle15 ]
+                        credhub_mode: skip-tests
+    "
+fi
 
-echo "::group::scf-config-values.yaml"
-cat "${GITHUB_WORKSPACE}/kubecf/dev/kubecf/kubecf-values.yaml"
+echo "::group::kubecf-config-values.yaml"
+cat "${GITHUB_WORKSPACE}/kubecf-values.yaml"
 echo "::endgroup::"
