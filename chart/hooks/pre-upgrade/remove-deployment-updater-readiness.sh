5c179de2e7ee126fddb8ca3b48c53f345f254659
viovanov <viovanov@suse.com>
2020-11-27 10:57:38 +0200

    Add support for upgrade and delete hooks
    Also adds a hook to patch the readiness probe for the deployment updater

---
+++ chart/hooks/pre-upgrade/remove-deployment-updater-readiness.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+patch="
+---
+spec:
+  template:
+    spec:
+      containers:
+      - name: cc-deployment-updater-cc-deployment-updater
+        readinessProbe:
+          $patch: delete
+"
+
+kubectl patch statefulset --namespace $NAMESPACE scheduler --patch "$patch"
