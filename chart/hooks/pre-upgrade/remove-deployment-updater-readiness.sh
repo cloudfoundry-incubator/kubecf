48b420a1c74abc3d9927c05bb9a8d116b1903f7a
Mark Yen <mark.yen@suse.com>
2020-12-02 14:10:07 -0800

    fix: chart/hooks: Delete scheduler pods on upgrade
    Even with the scheduler StatefulSet patched, existing scheduler pods
    will still have their readiness probes.  Due to the issue with their
    readiness probes (see previous commits), some of those scheduler pods
    will become unready during an upgrade; they will then block upgrade as
    new scheduler pods will not be spawned while some are unready.
    
    Work around the issue by clobbering the scheduler pods in the
    pre-upgrade hook; this induces downtime, but will let the upgrade
    proceed.

--- chart/hooks/pre-upgrade/remove-deployment-updater-readiness.sh
+++ chart/hooks/pre-upgrade/remove-deployment-updater-readiness.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-set -ex
+set -o errexit -o nounset -o pipefail -o xtrace
+
+# Remove the cc-deployment-updater readiness probes; they are incorrectly
+# constructed for an active/passive job, and cause the scheduler to show as
+# unready, and therefore blocking upgrades. See 
+# https://github.com/cloudfoundry-incubator/kubecf/issues/1589 for details.
+
+# Patch the StatefulSet so that any new instances we create will not have the
+# probe.
 
 # shellcheck disable=SC2016
 patch='
@@ -14,3 +22,9 @@ spec:
 '
 
 kubectl patch statefulset --namespace "$NAMESPACE" scheduler --patch "$patch"
+
+# Delete all existing scheduler pods; we can't just patch them as changing
+# existing readiness probes is not allowed.
+
+kubectl delete pods --namespace="${NAMESPACE}" \
+  --selector "quarks.cloudfoundry.org/instance-group-name=scheduler"
