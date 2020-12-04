ce51540633f1532376f60f3cf0c1258bdf73b8da
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2020-12-04 16:44:27 +0100

    Move wait-for-ip code to a standalone script in pull-request-ci/
    This reverts commit 519f6ae9fc21fa7fbac9c2dee162116d76d63689,
    adds .github/workflows/pull-request-ci/wait-for-ip.sh
    and consumes it on tests and upgrade-tests job
    
    The code only works for services that are LB/Ingress-terminated, not those with
    hardcoded IPs such as Minikube, so it can't be on kubecf-wait.sh.
    
    Another solution would be to add information of what kind of service termination
    we have to the cluster, so one can piece that together with the domain (e.g: by
    using a configmap containing the service type).

---
+++ .github/workflows/pull-request-ci/wait-for-ip.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# This script waits for services LB/Ingress-terminated (not with hardcoded IPs) to be accessible
+# by the IP they expose
+
+set -o errexit -o nounset
+
+selector=(
+      'app.kubernetes.io/component=router'
+      '!quarks.cloudfoundry.org/instance-group-name'
+    )
+jsonpath='{.items[].status.loadBalancer.ingress[].ip}'
+filter=(
+      --namespace=kubecf
+      --selector="$(IFS=, ; echo "${selector[*]}")"
+    )
+ip="$(kubectl get service "${filter[@]}" --output="jsonpath=${jsonpath}")"
+system_domain="$(gomplate --context ".=${GITHUB_WORKSPACE}/kubecf-values.yaml" --in '{{ .system_domain }}')"
+
+for host in api login; do
+    fqdn="${host}.${system_domain}"
+    printf "Waiting for %s to have IP address %s..." "${fqdn}" "${ip}"
+    while [[ "$(dig +short "${fqdn}")" != "${ip}" ]]; do
+        printf "."
+        sleep 10
+    done
+    printf "Done.\n"
+done
